611dd1245622c7e7c3a354916941eb73609c29ac
Maciej Hnat <maciej.hnat@infermedica.com>
2020-12-16 12:29:13 +0000

    SC-176: Create SimpleQuestion component
    * fix: Cr fixes
    
    * fix: Cr fixes to UiSimpleQuestion
    
    * fix: Remove duplicate spec
    
    * fix: Remove not needed slot
    
    * SC-176: Add story for slots, add specs and improve component
    
    * Merge branch 'master' into feature/SC-176_simple_question_component
    
    * Merge branch 'master' into feature/SC-176_simple_question_component
    
    * Simple question in progress
    
    * Merge branch 'master' into feature/SC-176_simple_question_component
    
    * Merge branch 'master' into feature/SC-176_simple_question_component
    
    Approved-by: Przemysław Spaczek

---
+++ src/components/organisms/UiSimpleQuestion/UiSimpleQuestion.spec.js
@@ -0,0 +1,42 @@
+import { mount } from '@vue/test-utils';
+import UiSimpleQuestion from './UiSimpleQuestion.vue';
+import UiTile from '../../molecules/UiTile/UiTile.vue';
+
+describe('UiSimpleQuestion.vue', () => {
+  test('renders a component', () => {
+    const wrapper = mount(UiSimpleQuestion);
+    expect(wrapper.classes('ui-simple-question')).toBe(true);
+  });
+  test('renders a component with correct amount of options', () => {
+    const options = [
+      {
+        id: 'present-id',
+        name: 'yes-no-dontknow',
+        label: 'Yes',
+        value: 'present',
+        iconAttrs: { icon: 'check' },
+      },
+      {
+        id: 'absent-id',
+        name: 'yes-no-dontknow',
+        label: 'No',
+        value: 'absent',
+        iconAttrs: { icon: 'cross' },
+      },
+      {
+        id: 'unknown-id',
+        name: 'yes-no-dontknow',
+        label: 'Dont know sometimes is long',
+        value: 'unknown',
+        iconAttrs: { icon: 'arrowRight' },
+      },
+    ];
+    const optionsLength = options.length;
+    const wrapper = mount(UiSimpleQuestion, {
+      props: { options },
+    });
+
+    const optionTiles = wrapper.findAllComponents(UiTile);
+    expect(optionTiles.length).toBe(optionsLength);
+  });
+});
---
+++ src/components/organisms/UiSimpleQuestion/UiSimpleQuestion.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="ui-simple-question">
+    <template v-for="option in options">
+      <!-- Use this slot to replace tile template -->
+      <slot
+        name="tile"
+        v-bind="{option}"
+      >
+        <UiTile
+          :id="option.id"
+          v-bind="tileAttrs"
+          :value="option.value"
+          :icon-attrs="option.iconAttrs"
+          :name="option.name"
+          :class="{'ui-tile--small': isTileSmall}"
+          class="ui-simple-question__option ui-tile"
+          @update:modelValue="updateHandler(option.value)"
+        >
+          {{ option.label }}
+        </UiTile>
+      </slot>
+    </template>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import UiTile from '../../molecules/UiTile/UiTile.vue';
+
+export default {
+  name: 'UiSimpleQuestion',
+  components: {
+    UiTile,
+  },
+  props: {
+    /**
+    * Use v-model to bind value to the component, example: <UiSimpleQuestion v-model="selected" />
+    */
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    /**
+    * Use this props to pass options for question
+    * example option:
+    * { id: 'id', name: 'option', label: 'label', value: 'value', iconAttrs: { icon: 'icon' }},
+    */
+    options: {
+      type: Array,
+      default: () => [],
+    },
+    /**
+    * Use this props to pass native attributes to all UiTiles
+    */
+    tileAttrs: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  emits: ['update:modelValue'],
+  setup(props, { attrs, emit }) {
+    const isTileSmall = computed(() => attrs.class?.includes('ui-simple-question--small'));
+
+    function updateHandler(value) {
+      emit('update:modelValue', value);
+    }
+
+    return {
+      isTileSmall,
+      updateHandler,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.ui-simple-question {
+  // ui-simple-question--small - utility class for small Tile variant
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 480px) {
+    flex-direction: row;
+  }
+
+  &__option {
+    margin: var(--simple-question-option-margin, 0 0 var(--space-12) 0);
+
+    @media (min-width: 480px) {
+      align-items: stretch;
+      width: 100%;
+      margin: var(--simple-question-option-tablet-margin, 0 var(--space-32) 0 0);
+      margin-bottom: 0;
+      text-align: center;
+    }
+
+    &:last-child {
+      --simple-question-option-margin: 0;
+      --simple-question-option-tablet-margin: 0;
+    }
+  }
+}
+</style>
+
