b560b86e40b7e579cb12c4e63d0a6c3e723fb226
Maciej Hnat <maciej.hnat@infermedica.com>
2020-12-22 08:27:30 +0000

    SC-305: Create MultipleAnswer component
    * Merge branch 'master' into feature/SC-305_multiple_answer_component
    
    * SC-305: Add emitting invalid event
    
    * Merge branch 'master' into feature/SC-305_multiple_answer_component
    
    * SC-305: Add option to set invalid and run lints
    
    * Merge branch 'master' into feature/SC-305_multiple_answer_component
    
    * Merge branch 'master' into feature/SC-305_multiple_answer_component
    
    * Merge branch 'master' into feature/SC-305_multiple_answer_component
    
    * SC-305: Refactor Multiple Answer validation without unit tests
    
    * SC-305: Refactor component tand remove required prop
    
    * SC-350: Update type of selected props
    
    Approved-by: Przemysław Spaczek

---
+++ src/components/organisms/UiMultipleAnswer/UiMultipleAnswer.spec.js
@@ -0,0 +1,143 @@
+import { mount } from '@vue/test-utils';
+import UiMultipleAnswer from './UiMultipleAnswer.vue';
+import UiCheckbox from '../../atoms/UiCheckbox/UiCheckbox.vue';
+import UiRadio from '../../atoms/UiRadio/UiRadio.vue';
+
+describe('UiMultipleAnswer.vue', () => {
+  const listItemErrorClass = '.ui-multiple-answer__list-item--has-error';
+  const choices = [
+    {
+      id: '6',
+      name: 'I have diabetes',
+      linked_observation: 'p_8',
+    },
+    {
+      id: '5',
+      name: 'I have hypertension',
+      linked_observation: 'p_9',
+    },
+    {
+      id: '4',
+      name: 'I have high cholesterol',
+      linked_observation: 'p_10',
+    },
+  ];
+  describe('Component with Radioses', () => {
+    test('renders a component with radio buttons when modelValue is a string', () => {
+      const selected = {};
+      const wrapper = mount(UiMultipleAnswer, {
+        props: {
+          modelValue: selected,
+          choices,
+        },
+      });
+      expect(wrapper.findComponent(UiRadio).exists()).toBe(true);
+    });
+    test('renders a component with radio buttons when modelValue is an object', () => {
+      const selected = {};
+      const wrapper = mount(UiMultipleAnswer, {
+        props: {
+          modelValue: selected,
+          choices,
+        },
+      });
+      expect(wrapper.findComponent(UiRadio).exists()).toBe(true);
+    });
+    test('renders a component with correct number of radioses', () => {
+      const selected = {};
+      const wrapper = mount(UiMultipleAnswer, {
+        props: {
+          modelValue: selected,
+          choices,
+        },
+      });
+      expect(wrapper.findAllComponents(UiRadio).length).toBe(choices.length);
+    });
+    test('renders a component and radioses with error styles when invalid and touched prop is true', () => {
+      const checkboxErrorClass = '.ui-checkbox--has-error';
+      const selected = [];
+      const wrapper = mount(UiMultipleAnswer, {
+        props: {
+          modelValue: selected,
+          invalid: true,
+          touched: true,
+          choices,
+        },
+      });
+      expect(wrapper.find(listItemErrorClass).exists()).toBe(true);
+      expect(wrapper.find(checkboxErrorClass).exists()).toBe(true);
+      expect(wrapper.findAll(listItemErrorClass).length).toBe(choices.length);
+      expect(wrapper.findAll(checkboxErrorClass).length).toBe(choices.length);
+    });
+    test('renders a component and radioses with error styles when invalid prop is false and touched prop is true', () => {
+      const checkboxErrorClass = '.ui-checkbox--has-error';
+      const selected = [];
+      const wrapper = mount(UiMultipleAnswer, {
+        props: {
+          modelValue: selected,
+          invalid: false,
+          touched: true,
+          choices,
+        },
+      });
+      expect(wrapper.find(listItemErrorClass).exists()).toBe(false);
+      expect(wrapper.find(checkboxErrorClass).exists()).toBe(false);
+      expect(wrapper.findAll(listItemErrorClass).length).toBe(0);
+      expect(wrapper.findAll(checkboxErrorClass).length).toBe(0);
+    });
+  });
+  describe('Component with Checkboxes', () => {
+    test('renders a component with checkboxes when modelValue is an array', () => {
+      const selected = [];
+      const wrapper = mount(UiMultipleAnswer, {
+        props: {
+          modelValue: selected,
+          choices,
+        },
+      });
+      expect(wrapper.findComponent(UiCheckbox).exists()).toBe(true);
+    });
+    test('renders a component correct number of checkboxes', () => {
+      const selected = [];
+      const wrapper = mount(UiMultipleAnswer, {
+        props: {
+          modelValue: selected,
+          choices,
+        },
+      });
+      expect(wrapper.findAllComponents(UiCheckbox).length).toBe(choices.length);
+    });
+    test('renders a component and checkboxes with error styles when invalid and touched prop is true', () => {
+      const radioErrorClass = '.ui-radio--has-error';
+      const selected = {};
+      const wrapper = mount(UiMultipleAnswer, {
+        props: {
+          modelValue: selected,
+          invalid: true,
+          touched: true,
+          choices,
+        },
+      });
+      expect(wrapper.find(listItemErrorClass).exists()).toBe(true);
+      expect(wrapper.find(radioErrorClass).exists()).toBe(true);
+      expect(wrapper.findAll(listItemErrorClass).length).toBe(choices.length);
+      expect(wrapper.findAll(radioErrorClass).length).toBe(choices.length);
+    });
+    test('renders a component and checkboxes with error styles when invalid prop is false and touched prop is true', () => {
+      const radioErrorClass = '.ui-radio--has-error';
+      const selected = {};
+      const wrapper = mount(UiMultipleAnswer, {
+        props: {
+          modelValue: selected,
+          invalid: false,
+          touched: true,
+          choices,
+        },
+      });
+      expect(wrapper.find(listItemErrorClass).exists()).toBe(false);
+      expect(wrapper.find(radioErrorClass).exists()).toBe(false);
+      expect(wrapper.findAll(listItemErrorClass).length).toBe(0);
+      expect(wrapper.findAll(radioErrorClass).length).toBe(0);
+    });
+  });
+});
---
+++ src/components/organisms/UiMultipleAnswer/UiMultipleAnswer.vue
@@ -0,0 +1,222 @@
+<template>
+  <UiList class="ui-multiple-answer">
+    <template
+      v-for="choice in choices"
+      :key="choice.id"
+    >
+      <!-- @slot Use this slot to replace list-item template.-->
+      <slot
+        name="list-item"
+        v-bind="{choice, updateHandler, errorClass, name, component}"
+      >
+        <UiListItem
+          class="ui-multiple-answer__list-item"
+          :class="{'ui-multiple-answer__list-item--has-error': hasError}"
+        >
+          <!-- @slot Use this slot to replace choice-item template.-->
+          <slot
+            name="choice-item"
+            v-bind="{choice, updateHandler, errorClass, name, component}"
+          >
+            <component
+              :is="component"
+              :id="choice.id"
+              :value="choice.linked_observation"
+              :name="name"
+              class="ui-multiple-answer__choice"
+              :class="errorClass"
+              @update:modelValue="updateHandler(choice.linked_observation)"
+            >
+              <template #label>
+                <!-- @slot Use this slot to replace choice-label template for specific item.-->
+                <slot
+                  :name="`label-${choice.linked_observation}`"
+                  v-bind="{choice}"
+                >
+                  <div class="ui-multiple-answer__label">
+                    <UiText
+                      tag="span"
+                    >
+                      {{ choice.name }}
+                    </UiText>
+                    <UiButton
+                      v-if="choice.explication"
+                      :aria-label="`${translation.explication} ${choice.name}`"
+                      class="ui-multiple-answer__explication ui-button--text ui-button--has-icon"
+                    >
+                      <UiIcon icon="info" />
+                    </UiButton>
+                  </div>
+                </slot>
+              </template>
+            </component>
+          </slot>
+        </UiListItem>
+      </slot>
+    </template>
+  </UiList>
+</template>
+
+<script>
+import { computed, watchEffect } from 'vue';
+import UiList from '../UiList/UiList.vue';
+import UiListItem from '../UiList/_internal/UiListItem.vue';
+import UiRadio from '../../atoms/UiRadio/UiRadio.vue';
+import UiText from '../../atoms/UiText/UiText.vue';
+import UiCheckbox from '../../atoms/UiCheckbox/UiCheckbox.vue';
+import UiButton from '../../atoms/UiButton/UiButton.vue';
+import UiIcon from '../../atoms/UiIcon/UiIcon.vue';
+
+export default {
+  name: 'UiMultipleAnswer',
+  components: {
+    UiRadio,
+    UiCheckbox,
+    UiListItem,
+    UiList,
+    UiText,
+    UiButton,
+    UiIcon,
+  },
+  props: {
+    /**
+     *  Use this props or v-model to set checked.
+     */
+    modelValue: {
+      type: [Object, Array],
+      default: () => ([]),
+    },
+    /**
+     *  Use this props to set possible choices.
+     */
+    choices: {
+      type: Array,
+      default: () => ([]),
+    },
+    /**
+     *  Use this props to set evidence source
+     */
+    source: {
+      type: String,
+      default: '',
+    },
+    /**
+     *  Use this props to group inputs with name attribute
+     */
+    name: {
+      type: String,
+      default: '',
+    },
+    /**
+     * Use this props to set invalid state of component.
+     */
+    invalid: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Use this props to touch component and show validation errors.
+     */
+    touched: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Use this props to override labels inside component translation.
+     */
+    translation: {
+      type: Object,
+      default: () => ({
+        explication: 'explication for',
+      }),
+    },
+  },
+  emits: ['update:modelValue', 'update:invalid'],
+  setup(props, { emit }) {
+    const component = computed(() => (Array.isArray(props.modelValue) ? 'ui-checkbox' : 'ui-radio'));
+    const isCheckbox = computed(() => (component.value === 'ui-checkbox'));
+    const valid = computed(() => {
+      if (!props.invalid) {
+        return true;
+      }
+      return isCheckbox.value
+        ? props.modelValue.length > 0
+        : props.modelValue.id;
+    });
+    const hasError = computed(() => (props.touched && !valid.value));
+    const errorClass = computed(() => (hasError.value ? `${component.value}--has-error` : ''));
+
+    watchEffect(() => {
+      if (!valid.value !== props.invalid) {
+        emit('update:invalid', !valid.value);
+      }
+    });
+
+    function updateHandler(value) {
+      if (!isCheckbox.value) {
+        emit('update:modelValue', { id: value, source: props.source, choice_id: 'present' });
+        return;
+      }
+      if (props.modelValue.some((evidence) => evidence.id === value)) {
+        emit('update:modelValue', props.modelValue.filter((evidence) => evidence.id !== value));
+      } else {
+        emit('update:modelValue', [...props.modelValue, { id: value, source: props.source, choice_id: 'present' }]);
+      }
+    }
+
+    return {
+      component,
+      hasError,
+      errorClass,
+      updateHandler,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../../styles/mixins/_mixins.scss';
+
+.ui-multiple-answer {
+  &__list-item {
+    --list-item-padding: 0;
+
+    background: var(--multiple-answer-list-item-background);
+    border: var(--multiple-answer-list-item-border, solid var(--color-border-subtle));
+    border-width: var(--multiple-answer-list-item-border-width, 1px 0 0 0);
+
+    &:last-of-type {
+      border-width: var(--multiple-answer-list-item-border-width, 1px 0);
+    }
+
+    &:hover {
+      background: var(--multiple-answer-list-item-hover-background);
+
+      @media (min-width: 480px) {
+        background: var(--multiple-answer-list-item-tablet-hover-background, var(--color-gray-50));
+      }
+    }
+
+    &--has-error {
+      --multiple-answer-list-item-background: var(--color-background-alert-error);
+    }
+  }
+
+  &__choice {
+    width: 100%;
+    padding: var(--multiple-answer-choice-padding, var(--space-12));
+  }
+
+  &__label {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  &__explication {
+    margin: var(--multiple-answer-margin, 0 0 0 var(--space-12));
+  }
+}
+</style>
+
