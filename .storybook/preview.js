54efb76d09b74667617c28476a80157b96fb6245
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2021-04-21 15:24:38 +0200

    Use vue-router to transform router-link to anchor element

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,13 @@
 import './tailwindcss.css';
 import '@/styles/styles.scss';
+import { createRouter, createWebHistory } from 'vue-router';
+import { app } from '@storybook/vue3';
+const routes = []
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+app.use(router);
 document.body.onload = function() {
   document.body.setAttribute('dir', 'ltr')
 };
