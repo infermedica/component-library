73a277c1bce0c95907db105e071a43de10c6d668
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2022-03-08 14:18:51 +0100

    v0.2.7

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
     '@vue/typescript/recommended',
+    'plugin:storybook/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -21,30 +22,26 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'import/first': 'off', // scrip setup related
-    'no-multiple-empty-lines': [
-      'error',
-      {
-        max: 1,
-        maxEOF: 1,
-      },
-    ],
+    'import/first': 'off',
+    // scrip setup related
+    'no-multiple-empty-lines': ['error', {
+      max: 1,
+      maxEOF: 1,
+    }],
     'max-len': ['off'],
-    'vue/max-len': ['error',
-      {
-        code: 120,
-        template: 120,
-        tabWidth: 2,
-        ignoreComments: true,
-        ignoreTrailingComments: true,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
-        ignoreHTMLAttributeValues: true,
-        ignoreHTMLTextContents: true,
-      },
-    ],
+    'vue/max-len': ['error', {
+      code: 120,
+      template: 120,
+      tabWidth: 2,
+      ignoreComments: true,
+      ignoreTrailingComments: true,
+      ignoreUrls: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+      ignoreRegExpLiterals: true,
+      ignoreHTMLAttributeValues: true,
+      ignoreHTMLTextContents: true,
+    }],
     'vue/max-attributes-per-line': ['error', {
       singleline: {
         max: 1,
@@ -54,23 +51,16 @@ module.exports = {
       },
     }],
     'import/no-extraneous-dependencies': ['error', {
-      devDependencies: [
-        '**/*.stories.@(js|jsx|ts|tsx|mdx)',
-        '**/*.spec.{j,t}s?(x)',
-      ],
+      devDependencies: ['**/*.stories.@(js|jsx|ts|tsx|mdx)', '**/*.spec.{j,t}s?(x)'],
     }],
     'prefer-destructuring': ['error', {
       array: false,
     }],
   },
-  overrides: [
-    {
-      files: [
-        '**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
+  overrides: [{
+    files: ['**/*.spec.{j,t}s?(x)'],
+    env: {
+      jest: true,
     },
-  ],
+  }],
 };
