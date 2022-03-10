1fddf33d3af2b41588b5e7b548c160de4c02b013
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2022-03-10 09:17:48 +0000

    fix: Use updated value in highlight directive
    * fix: Use updated value in highlight directive
    
    
    Approved-by: Mateusz Kulpa
    Approved-by: Kuba Marek

--- src/utilities/directives/highlight/index.js
+++ src/utilities/directives/highlight/index.js
@@ -1,11 +1,10 @@
 /* eslint-disable import/prefer-default-export, no-underscore-dangle, no-param-reassign, func-names */
 export const highlight = {
   beforeMount(el, binding) {
-    el.__highlightHandler__ = function (element) {
-      const query = binding.value;
+    el.__highlightHandler__ = function (element, { value }) {
       const content = element.innerHTML.replace(/<\/?mark>/gi, '');
       const marked = content.replace(
-        new RegExp(query, 'gi'),
+        new RegExp(value, 'gi'),
         (match) => `<mark>${match}</mark>`,
       );
       element.innerHTML = marked;
