08e6a965dec4f823501f1af3e77240e670c84ac7
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2022-04-19 11:24:19 +0200

    docs: add group stories for switch component

--- src/components/molecules/UiSwitch/UiSwitch.stories.js
+++ src/components/molecules/UiSwitch/UiSwitch.stories.js
@@ -2,6 +2,8 @@ import UiSwitch from '@/components/molecules/UiSwitch/UiSwitch.vue';
 import UiCheckbox from '@/components/atoms/UiCheckbox/UiCheckbox.vue';
 import UiSwitchControl from '@/components/molecules/UiSwitch/_internal/UiSwitchControl.vue';
 import UiText from '@/components/atoms/UiText/UiText.vue';
+import UiList from '@/components/organisms/UiList/UiList';
+import UiListItem from '@/components/organisms/UiList/_internal/UiListItem';
 import { actions } from '@storybook/addon-actions';
 import { ref } from 'vue';
 import { content, disabled } from '@sb/helpers/argTypes';
@@ -117,3 +119,101 @@ export const SlotsInSwitch = (args) => ({
     {{ content }}
   </UiSwitch>`,
 });
+
+export const AsGroup = (args) => ({
+  components: { UiSwitch, UiList, UiListItem },
+  setup() {
+    const modelValue = ref(args.initModelValue);
+    return { ...args, modelValue };
+  },
+  template: `<UiList style="--list-item-padding: var(--space-12) 0;">
+    <UiListItem
+      v-for="(value, key) in values"
+      :key="key"
+    >
+      <UiSwitch
+        v-model="modelValue"
+        :value="value"
+      >
+        {{value.label}}
+      </UiSwitch>
+    </UiListItem>
+  </UiList>`,
+});
+AsGroup.args = {
+  initModelValue: [{
+    label: 'Necessary',
+    id: 'necessary',
+  }],
+  values: [
+    {
+      label: 'Necessary',
+      id: 'necessary',
+    },
+    {
+      label: 'Functional',
+      id: 'functional',
+    },
+    {
+      label: 'Analytics',
+      id: 'analytics',
+    },
+  ],
+};
+AsGroup.argTypes = {
+  initial: {
+    description: 'Use this control to set initial state.',
+    table: {
+      category: 'stories controls',
+    },
+    control: 'array',
+  },
+  values: {
+    description: 'Use this control to set the values of checkbox group.',
+    table: {
+      category: 'stories controls',
+    },
+    control: 'array',
+  },
+};
+
+export const AsGroupWithPrimitiveTypes = (args) => ({
+  components: { UiSwitch, UiList, UiListItem },
+  setup() {
+    const modelValue = ref(args.initModelValue);
+    return { ...args, modelValue };
+  },
+  template: `<UiList style="--list-item-padding: var(--space-12) 0;">
+    <UiListItem
+      v-for="(value, key) in values"
+      :key="key"
+    >
+      <UiSwitch
+        v-model="modelValue"
+        :value="value"
+      >
+        {{value}}
+      </UiSwitch>
+    </UiListItem>
+  </UiList>`,
+});
+AsGroupWithPrimitiveTypes.args = {
+  initModelValue: ['Necessary'],
+  values: ['Necessary', 'Functional', 'Analytics'],
+};
+AsGroupWithPrimitiveTypes.argTypes = {
+  initial: {
+    description: 'Use this control to set initial state.',
+    table: {
+      category: 'stories controls',
+    },
+    control: 'array',
+  },
+  values: {
+    description: 'Use this control to set the values of checkbox group.',
+    table: {
+      category: 'stories controls',
+    },
+    control: 'array',
+  },
+};
