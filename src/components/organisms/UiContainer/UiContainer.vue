3464af39a6734b8aa45192df40301ebebe5cafa4
Arkadiusz Krzymieński <arkadiusz.krzymienski@infermedica.com>
2022-05-06 09:42:51 +0000

    fix(style): padding in UiContainer component
    * style: unify paddings in UiContainer
    
    
    Approved-by: Przemysław Spaczek

--- src/components/organisms/UiContainer/UiContainer.vue
+++ src/components/organisms/UiContainer/UiContainer.vue
@@ -16,12 +16,18 @@ export default {
 
 .ui-container {
   padding: var(--container-padding, var(--space-40) var(--space-32));
-  background: var(--containter-background, var(--color-white));
+  background: var(--container-background, var(--color-white));
   border-radius: var(--container-border-radius, 0);
   box-shadow: var(--box-shadow-low);
 
   @include from-tablet {
+    --container-padding: var(--space-48);
+
     border-radius: var(--container-tablet-border-radius, var(--border-radius-container));
+  }
+
+  @include from-desktop {
+    --container-padding: var(--space-48) var(--space-64);
   }
 }
 </style>
