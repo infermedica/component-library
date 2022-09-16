76673842c0f953f53b3bac4df13be4de996a53dd
Arkadiusz Krzymieński <82519187+ArkadiuszKrzymienski@users.noreply.github.com>
2022-09-16 18:22:01 +0200

    refactor: rewrite UILoader
    * v0.5.1
    
    * fix: rewrite UiLoader component
    
    * feat: add isLoading switcher and update loaded content
    
    * refactor: custom and native transition component
    
    * feat: add opacity mode, add stories
    
    * feat: a few small improvements
    
    * fix: remove unused v transition
    
    Co-authored-by: Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -52,7 +52,6 @@ export const globalTypes = {
         {value: 'brand', title: 'Brand'},
       ],
       dynamicTitle: true,
-      showName: true,
     }
   }
 }
---
+++ src/components/molecules/UiLoader/_internal/UiLoaderNativeTransition.vue
@@ -0,0 +1,23 @@
+<template>
+  <transition>
+    <slot
+      v-if="isLoading"
+      name="loader"
+    />
+    <div v-else>
+      <slot />
+    </div>
+  </transition>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  /**
+   * Use this props to show UiLoader component
+   */
+  isLoading: {
+    type: Boolean,
+    default: true,
+  },
+});
+</script>
