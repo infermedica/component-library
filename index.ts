4fe2a3e894d0beb1b064f7d1f67c21f120f1a6de
Arkadiusz Krzymieński <82519187+ArkadiuszKrzymienski@users.noreply.github.com>
2022-09-27 11:18:57 +0200

    feat: husky pre-push hook
    * v0.5.1
    
    * ci: commit auto-generated files on pre-push hook
    
    * chore: update auto-generated files
    
    * ci: remove file generation before storybook build
    
    * chore: update auto-generated files
    
    * chore: remove unnecessary scripts
    
    Co-authored-by: Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>

---
+++ index.ts
@@ -0,0 +1,96 @@
+// Auto-generated file by create-index.js. Do not edit manually
+import UiBackdrop from './src/components/atoms/UiBackdrop/UiBackdrop.vue';
+import UiButton from './src/components/atoms/UiButton/UiButton.vue';
+import UiCheckbox from './src/components/atoms/UiCheckbox/UiCheckbox.vue';
+import UiHeading from './src/components/atoms/UiHeading/UiHeading.vue';
+import UiIcon from './src/components/atoms/UiIcon/UiIcon.vue';
+import UiInput from './src/components/atoms/UiInput/UiInput.vue';
+import UiLink from './src/components/atoms/UiLink/UiLink.vue';
+import UiProgress from './src/components/atoms/UiProgress/UiProgress.vue';
+import UiRadio from './src/components/atoms/UiRadio/UiRadio.vue';
+import UiRange from './src/components/atoms/UiRange/UiRange.vue';
+import UiText from './src/components/atoms/UiText/UiText.vue';
+import UiTextarea from './src/components/atoms/UiTextarea/UiTextarea.vue';
+import UiAlert from './src/components/molecules/UiAlert/UiAlert.vue';
+import UiBulletPoints from './src/components/molecules/UiBulletPoints/UiBulletPoints.vue';
+import UiChip from './src/components/molecules/UiChip/UiChip.vue';
+import UiDropdown from './src/components/molecules/UiDropdown/UiDropdown.vue';
+import UiFormField from './src/components/molecules/UiFormField/UiFormField.vue';
+import UiHeader from './src/components/molecules/UiHeader/UiHeader.vue';
+import UiInteractiveSvg from './src/components/molecules/UiInteractiveSvg/UiInteractiveSvg.vue';
+import UiLoader from './src/components/molecules/UiLoader/UiLoader.vue';
+import UiNavigation from './src/components/molecules/UiNavigation/UiNavigation.vue';
+import UiNotification from './src/components/molecules/UiNotification/UiNotification.vue';
+import UiNumberStepper from './src/components/molecules/UiNumberStepper/UiNumberStepper.vue';
+import UiPopover from './src/components/molecules/UiPopover/UiPopover.vue';
+import UiProgressbar from './src/components/molecules/UiProgressbar/UiProgressbar.vue';
+import UiRating from './src/components/molecules/UiRating/UiRating.vue';
+import UiScale from './src/components/molecules/UiScale/UiScale.vue';
+import UiStepper from './src/components/molecules/UiStepper/UiStepper.vue';
+import UiSwitch from './src/components/molecules/UiSwitch/UiSwitch.vue';
+import UiTile from './src/components/molecules/UiTile/UiTile.vue';
+import UiToggleButtonGroup from './src/components/molecules/UiToggleButtonGroup/UiToggleButtonGroup.vue';
+import UiAccordion from './src/components/organisms/UiAccordion/UiAccordion.vue';
+import UiCard from './src/components/organisms/UiCard/UiCard.vue';
+import UiContainer from './src/components/organisms/UiContainer/UiContainer.vue';
+import UiControls from './src/components/organisms/UiControls/UiControls.vue';
+import UiDatepicker from './src/components/organisms/UiDatepicker/UiDatepicker.vue';
+import UiList from './src/components/organisms/UiList/UiList.vue';
+import UiMegaMenu from './src/components/organisms/UiMegaMenu/UiMegaMenu.vue';
+import UiModal from './src/components/organisms/UiModal/UiModal.vue';
+import UiMultipleAnswer from './src/components/organisms/UiMultipleAnswer/UiMultipleAnswer.vue';
+import UiMultipleChoices from './src/components/organisms/UiMultipleChoices/UiMultipleChoices.vue';
+import UiSidePanel from './src/components/organisms/UiSidePanel/UiSidePanel.vue';
+import UiSimpleQuestion from './src/components/organisms/UiSimpleQuestion/UiSimpleQuestion.vue';
+import UiTabs from './src/components/organisms/UiTabs/UiTabs.vue';
+import UiMessage from './src/components/templates/UiMessage/UiMessage.vue';
+import UiQuestion from './src/components/templates/UiQuestion/UiQuestion.vue';
+
+export {
+  UiBackdrop,
+  UiButton,
+  UiCheckbox,
+  UiHeading,
+  UiIcon,
+  UiInput,
+  UiLink,
+  UiProgress,
+  UiRadio,
+  UiRange,
+  UiText,
+  UiTextarea,
+  UiAlert,
+  UiBulletPoints,
+  UiChip,
+  UiDropdown,
+  UiFormField,
+  UiHeader,
+  UiInteractiveSvg,
+  UiLoader,
+  UiNavigation,
+  UiNotification,
+  UiNumberStepper,
+  UiPopover,
+  UiProgressbar,
+  UiRating,
+  UiScale,
+  UiStepper,
+  UiSwitch,
+  UiTile,
+  UiToggleButtonGroup,
+  UiAccordion,
+  UiCard,
+  UiContainer,
+  UiControls,
+  UiDatepicker,
+  UiList,
+  UiMegaMenu,
+  UiModal,
+  UiMultipleAnswer,
+  UiMultipleChoices,
+  UiSidePanel,
+  UiSimpleQuestion,
+  UiTabs,
+  UiMessage,
+  UiQuestion,
+};
