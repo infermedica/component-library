6b0cf4626bacca51d6ae4950773cc0baae389830
Arkadiusz Krzymieński <a.krzymienski@gmail.com>
2022-10-07 15:57:46 +0200

    feat: add vue-docgen-api to trace changes

--- scripts/helpers/paths-components-loop.js
+++ scripts/helpers/paths-components-loop.js
@@ -9,17 +9,17 @@ const pathsVueComponents = glob.sync('*/*/Ui*.vue', {
 const pathsVueInternalComponents = glob.sync('*/*/_internal/{Ui*.vue,*/Ui*.vue}', {
   cwd: pathComponentsRoot,
 });
+
 const pathsComponentsLoop = (callbacks, withInternals = true) => {
   const pathsComponents = withInternals ? [...pathsVueComponents, ...pathsVueInternalComponents] : pathsVueComponents;
-  pathsComponents.forEach(((pathComponent) => {
+  pathsComponents.forEach((pathComponent) => {
     if (Array.isArray(callbacks)) {
       callbacks.forEach((callback) => {
         callback(pathComponent);
       });
-      return;
     }
     callbacks(pathComponent);
-  }));
+  });
 };
 
 module.exports = pathsComponentsLoop;
