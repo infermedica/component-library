95c8de2c1ea17ccfd788dacc673733cb249274de
Alan Leszczuk <alan.leszczuk@infermedica.com>
2022-10-23 17:47:16 +0200

    feat: issue-9: add css variables to styles

--- src/components/organisms/UiPhoneNumber/UiPhoneNumber.vue
+++ src/components/organisms/UiPhoneNumber/UiPhoneNumber.vue
@@ -14,13 +14,13 @@
       <fieldset class="ui-phone-number__fields">
         <UiText
           tag="legend"
-          class="ui-form-field__label"
+          class="ui-form-field__label ui-phone-number__label"
         >
           <UiText
             class="ui-form-field__label-text"
             tag="span"
           >
-            {{ label }}
+            {{ translation.phoneNumberLabel }}
           </UiText>
         </UiText>
         <slot
@@ -35,7 +35,7 @@
             id="ui-phone-number__dropdown"
             class="ui-phone-number__dropdown-form-field"
             :error-message="false"
-            :label="countryCodeLabel"
+            :label="translation.countryCodeLabel"
             :text-message-attrs="{ class: 'visual-hidden' }"
             :text-hint-attrs="{ class: 'visual-hidden' }"
           >
@@ -44,6 +44,7 @@
                 id="ui-phone-number__dropdown"
                 class="ui-phone-number__dropdown"
                 :model-value="selected"
+                :popover-attrs="{ class: 'ui-phone-number__dropdown-popover' }"
                 @update:model-value="handleOnSelected"
               >
                 <template
@@ -76,7 +77,7 @@
                 >
                   <UiDropdownItem
                     :value="option"
-                    class="ui-dropdown-item--compact"
+                    class="ui-dropdown-item--compact ui-phone-number__dropdown-item"
                   >
                     {{ option.country }} ({{ option.code }})
                   </UiDropdownItem>
@@ -98,7 +99,7 @@
             id="ui-phone-number__input"
             class="ui-phone-number__input-form-field"
             :error-message="error"
-            :label="label"
+            :label="translation.phoneNumberLabel"
             :text-message-attrs="{ class: 'visual-hidden' }"
             :text-hint-attrs="{ class: 'visual-hidden' }"
           >
@@ -108,7 +109,7 @@
                 v-model="phone"
                 class="ui-phone-number__input"
                 :class="{ 'ui-input--has-error': error, }"
-                :placeholder="placeholder"
+                :placeholder="translation.phoneNumberPlaceholder"
                 type="number"
                 @blur="handleOnBlur"
               />
@@ -147,6 +148,14 @@ import { getPhoneCodes } from '../../../utilities/helpers';
 import type { PhoneCodeType } from '../../../utilities/helpers';
 import type { UiPhoneNumberProps } from './types';
 
+export interface PhoneNumberTranslation {
+  phoneNumberLabel: string, /** Use this prop to set the label text */
+  phoneNumberPlaceholder: string, /** Use this props to set the input's placeholder */
+  countryCodeLabel: string, /** Use this prop to set the hidden country-code label text */
+  errorMessage: string, /** Use this props to set alert message */
+  hint: boolean | string, /** Use this props to set label hint, ex: "Required" or "Optional" */
+}
+
 const emit = defineEmits([
   'update:modelValue',
   'update:touched',
@@ -155,32 +164,17 @@ const emit = defineEmits([
 
 const props: UiPhoneNumberProps = defineProps({
   /**
-   * Use this prop to set the label text.
+   * Use this props to override labels inside component translation.
    */
-  label: {
-    type: String as PropType<string>,
-    default: 'Phone number',
-  },
-  /**
-   * Use this prop to set the hidden country-code label text.
-   */
-  countryCodeLabel: {
-    type: String as PropType<string>,
-    default: 'Country code',
-  },
-  /**
-   * Use this props to set the input's placeholder
-   */
-  placeholder: {
-    type: String as PropType<string>,
-    default: 'Put your phone number',
-  },
-  /**
-   * Use this props to set alert message
-   */
-  errorMessage: {
-    type: String as PropType<string>,
-    default: 'Error message',
+  translation: {
+    type: Object as PropType<PhoneNumberTranslation>,
+    default: () => ({
+      phoneNumberLabel: 'Phone number',
+      phoneNumberPlaceholder: 'Put your phone number',
+      countryCodeLabel: 'Country code',
+      errorMessage: 'Error message',
+      hint: false,
+    }),
   },
   /**
    * Use this props to control the phone-number value
@@ -209,16 +203,6 @@ const props: UiPhoneNumberProps = defineProps({
   defaultCountryCode: {
     type: String as PropType<string>,
     default: 'US',
-  },
-  /**
-   * Use this props to set label hint, ex: "Required" or "Optional"
-   */
-  hint: {
-    type: [
-      Boolean,
-      String,
-    ] as PropType<boolean | string>,
-    default: false,
   },
 });
 
@@ -299,84 +283,99 @@ onMounted(async () => {
   }
 
   &__fields {
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    //gap: functions.var($element + "-fields-mobile", gap, 0));
+    display: functions.var($element + "-fields-mobile", display, flex);
+    width: functions.var($element + "-fields-mobile", width, 100%);
+    flex-direction: functions.var($element + "-fields-mobile", flex-direction, column);
 
     @include mixins.from-tablet {
-      flex-direction: row;
+      flex-direction: functions.var($element + "-fields-desktop", flex-direction, row);
       gap: functions.var($element + "-fields-desktop", gap, var(--space-4));
     }
 
-    #{$this}__dropdown {
-      --button-padding: var(--space-12);
-      --button-icon-margin: 0 var(--space-4) 0 var(--space-8);
+    #{$this}__label {
+      margin: functions.var($element + "-label", margin, 0);
+    }
 
-      font: var(--font-body-1);
-      width: 100%;
+    #{$this}__dropdown-form-field {
+      flex-grow: functions.var($element + "-dropdown-form-field", flex-grow, 1);
 
-      &-button {
-        --button-color: var(--color-text-body);
-        --button-hover-color: var(--color-text-body);
-        --button-active-color: var(--color-text-body);
-        --button-border-color: var(--color-border-strong);
-        --button-hover-border-color: var(--color-border-strong-hover);
-        --button-padding: var(--space-12) var(--space-12) var(--space-12) var(--space-16);
-        --button-background: var(--color-background-white);
-        --button-hover-background: var(--color-background-white);
-        --button-active-background: var(--color-background-white);
-        --icon-color: var(--color-icon-secondary);
-        --button-icon-color-active: var(--color-icon-secondary-active);
-        --button-icon-color-hover: var(--color-icon-secondary-hover);
-        --button-icon-margin: 0;
-        --button-width: 100%;
+      #{$this}__dropdown {
+        --button-padding: var(--space-12);
+        --button-icon-margin: 0 var(--space-4) 0 var(--space-8);
 
-        justify-content: space-between;
-        font: var(--font-body-1);
-      }
+        font: functions.var($element + "-dropdown", font, var(--font-body-1));
+        width: functions.var($element + "-dropdown", width, 100%);
 
-      .ui-dropdown-item {
-        text-align: left;
-        white-space: break-spaces;
-      }
+        &-button {
+          --button-color: var(--color-text-body);
+          --button-hover-color: var(--color-text-body);
+          --button-active-color: var(--color-text-body);
+          --button-border-color: var(--color-border-strong);
+          --button-hover-border-color: var(--color-border-strong-hover);
+          --button-padding: var(--space-12) var(--space-12) var(--space-12) var(--space-16);
+          --button-background: var(--color-background-white);
+          --button-hover-background: var(--color-background-white);
+          --button-active-background: var(--color-background-white);
+          --icon-color: var(--color-icon-secondary);
+          --button-icon-color-active: var(--color-icon-secondary-active);
+          --button-icon-color-hover: var(--color-icon-secondary-hover);
+          --button-icon-margin: 0;
+          --button-width: 100%;
 
-      .ui-popover {
-        --popover-border: none;
+          justify-content: functions.var($element + "-dropdown-button", justify-content, space-between);
+          font: functions.var($element + "-dropdown-button", font, var(--font-body-1));
+          flex-grow: functions.var($element + "-dropdown-button", flex-grow, 1);
+        }
 
-        width: 100%;
-        min-height: 200px;
-        max-height: calc(100vh - (v-bind(popoverOffsetTop) * 1px)); /* stylelint-disable-line value-keyword-case */
-        overflow-y: scroll;
-        border: 1px solid var(--color-border-subtle);
-      }
+        &-popover {
+          --popover-border: none;
+          --dropdown-popover-width: 100%;
+          --dropdown-popover-max-width: none;
+          --dropdown-popover-min-height: 200px;
 
-      #{$this}__dropdown-text {
-        &--mobile {
-          white-space: pre-wrap;
-          flex-grow: 1;
-          text-align: left;
+          overflow-y: functions.var($element + "-dropdown-popover", overflow-y, scroll);
+          border: functions.var($element + "-dropdown-popover", border, 1px solid var(--color-border-subtle));
+          max-height: functions.var($element + "-dropdown-popover-mobile", max-height, calc(100vh - (v-bind(popoverOffsetTop) * 1px))); /* stylelint-disable-line value-keyword-case */
+          z-index: functions.var($element + "-dropdown-popover", z-index, 1);
 
           @include mixins.from-tablet {
-            display: none;
+            --dropdown-popover-max-width: 320px;
+            --dropdown-popover-width: 320px;
+
+            max-height: functions.var($element + "-dropdown-popover-desktop", max-height, calc(100vh - (v-bind(popoverOffsetTop) * 1px))); /* stylelint-disable-line value-keyword-case */
           }
         }
 
-        &--desktop {
-          white-space: pre-wrap;
-          flex-grow: 1;
-          text-align: left;
+        &-item {
+          text-align: functions.var($element + "-dropdown-item", text-align, left);
+          white-space: functions.var($element + "-dropdown-item", white-space, break-spaces);
+        }
 
-          @include mixins.to-mobile {
-            display: none;
+        #{$this}__dropdown-text {
+          &--mobile {
+            white-space: functions.var($element + "-dropdown-text-mobile", white-space, pre-wrap);
+            flex-grow: functions.var($element + "-dropdown-text-mobile", flex-grow, 1);
+            text-align: functions.var($element + "-dropdown-text-mobile", text-align, left);
+
+            @include mixins.from-tablet {
+              display: none;
+            }
+          }
+
+          &--desktop {
+            @include mixins.to-mobile {
+              display: none;
+            }
           }
         }
       }
     }
 
     #{$this}__input-form-field {
+      flex-grow: functions.var($element + "-input-form-field", flex-grow, 11);
+
       #{$this}__input {
-        width: 100%;
+        width: functions.var($element + "-input", width, 100%);
       }
     }
   }
