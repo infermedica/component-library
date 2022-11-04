74c78c6869330bdf1a794992948a46fd5dda8ace
Arkadiusz Krzymieński <a.krzymienski@gmail.com>
2022-11-04 13:49:46 +0100

    feat: add custom sort for stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,22 +12,92 @@ SyntaxHighlighter.registerLanguage('scss', scss);
 
 export const parameters = {
   options: {
-    storySort: {
-      order: [
-        'Welcome',
-        'Getting Started',
-        ['Development Guide', ['Installation', ['*']]],
-        'Releases',
-        // auto-generated release stories order
-        ["v0.x.x",["v0.5.x",["v0.5.2","v0.5.1","v0.5.0"],"v0.4.x",["v0.4.3","v0.4.2","v0.4.1","v0.4.0"],"v0.3.x",["v0.3.4","v0.3.3","v0.3.2","v0.3.1","v0.3.0"],"v0.2.x",["v0.2.7","v0.2.6","v0.2.5","v0.2.4","v0.2.3","v0.2.2","v0.2.1","v0.2.0"],"v0.1.x",["v0.1.1","v0.1.0","v0.1.0-beta2","v0.1.0-beta1"],"v0.0.x",["v0.0.1-beta4"]]],
-        'Atoms',
-        'Molecules',
-        'Organisms',
-        'Templates',
-        'Utilities',
-        ['Directives', ['Docs', '*']],
-      ]
-    },
+    storySort: (story1, story2) => {
+      /* 
+        Sort function to sort stories based on a config file.
+        {storiesOrder} is the order in which we want stories to appear.
+        Any stories that aren't explicitly listed will appear at the end of list in alphabetical order.
+        Modifiers: 
+          {'*'} - every nested element
+          {'rest-reverse'} - rest of not listed elements appear in reverse alphabetical order
+      */
+      const storiesOrder = {
+        // Keys must be in camelCase
+        welcome: {},
+        team: {},
+        gettingStarted: {
+          developmentGuide: {
+            installation: {},
+          },
+        },
+        releases: {
+          '*': {
+            'rest-reverse': {
+              'rest-reverse': {
+                changeLog: {}
+              },
+            }
+          },
+        },
+        atoms: {
+          '*': {
+            common: {}
+          },
+        },
+        molecules: {
+          '*': {
+            common: {}
+          },
+        },
+        organisms: {
+          '*': {
+            common: {}
+          },
+        },
+        templates: {
+          '*': {
+            common: {}
+          },
+        },
+        utilities: {
+          directives: {
+            docs: {}
+          },
+        },
+        contributingGuide: {}
+      };
+      const hasKey = (obj, key) => Object.hasOwn(obj, key);
+      const compareAlphabetical = (a, b) => a.localeCompare(b, { numeric: true });
+      const toCamelCase = (str) => str.toLowerCase().replace(/[^a-zA-Z0-9.-]+(.)/g, (m, chr) => chr.toUpperCase())
+      const getStoryPath = (story) => [...story.title.split('/'), story.name].map(key => toCamelCase(key));
+      const story1Path = getStoryPath(story1);
+      const story2Path = getStoryPath(story2);
+      const compareStoryPaths = (order, path1, path2) => {
+        if (!path1.length || !path2.length) return path1.length - path2.length;
+        const [path1Head, ...path1Tail] = path1;
+        const [path2Head, ...path2Tail] = path2;
+        if (!order || 'rest-reverse' in order && !hasKey(order, path1Head)) {
+          const reverseOrder = order && order['rest-reverse'];
+          const comp = compareAlphabetical(path1Head, path2Head);
+          if (comp === 0) return compareStoryPaths(reverseOrder, path1Tail, path2Tail);
+          return reverseOrder ? comp * -1 : comp
+        }
+        if (path1Head === path2Head) {
+          return compareStoryPaths(order[path1Head] || order['*'], path1Tail, path2Tail);
+        }
+        if (hasKey(order, path1Head) && hasKey(order, path2Head)) {
+          const orderKeys = Object.keys(order);
+          return orderKeys.indexOf(path1Head) - orderKeys.indexOf(path2Head);
+        } else if (hasKey(order, path1Head) && !hasKey(order, path2Head)) {
+          return -1;
+        } else if (!hasKey(order, path1Head) && hasKey(order, path2Head)) {
+          return 1;
+        } else {
+          return compareStoryPaths(order['*'], path1, path2);
+        }
+      }
+      return compareStoryPaths(storiesOrder, story1Path, story2Path)
+    }
   },
   viewMode: 'docs',
   backgrounds: {
