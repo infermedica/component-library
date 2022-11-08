0f4ab1a190fe69df28d66cae057a4beea728d488
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2022-11-08 13:03:07 +0100

    test: add unit test for UiQuestion

--- src/components/templates/UiQuestion/UiQuestion.spec.ts
+++ src/components/templates/UiQuestion/UiQuestion.spec.ts
@@ -1,10 +1,23 @@
 import { mount } from '@vue/test-utils';
+import UiButton from '@/components/atoms/UiButton/UiButton.vue';
 import UiQuestion from './UiQuestion.vue';
 
 describe('UiQuestion.vue', () => {
   test('renders a component', () => {
     const wrapper = mount(UiQuestion);
     expect(wrapper.classes('ui-question')).toBe(true);
+  });
+  test('component does not render skip button when buttonActionAttrs is empty', () => {
+    const settings = {
+      issue: {
+        action: true,
+        feedback: true,
+        skip: false,
+      },
+    };
+    const wrapper = mount(UiQuestion, { propsData: { settings } });
+    const notification = wrapper.find('.ui-question__feedback');
+    expect(notification.findComponent(UiButton).exists()).toBe(false);
   });
   test('component render skip button', () => {
     const settings = {
