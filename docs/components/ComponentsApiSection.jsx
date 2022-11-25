252f23ad84669e17eb2177501a0d158c8eddd583
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2022-11-25 17:17:36 +0100

    refactor: update output of Components Api Section component

--- docs/components/ComponentsApiSection.jsx
+++ docs/components/ComponentsApiSection.jsx
@@ -2,32 +2,42 @@
 import React from 'react';
 import { styled } from '@storybook/theming';
 import { Table } from './Table';
+import { capitalizeFirst } from '../../src/utilities/helpers/index';
 
-const Section = styled.section`
-  font-family: "Nunito Sans", -apple-system, ".SFNSText-Regular", "San Francisco", BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;`;
 const List = styled.ul`
   list-style-type: none;
   padding: 0 8px;`;
 const ListItem = styled.ul`
-  padding: 8px 0;
+  padding: 0;
   margin: 0;`;
 const SectionTitle = styled.h2`
-  margin: 20px 0 16px;
+  margin: 20px 0 8px;
   padding-bottom: 4px;
-  border-bottom: 1px solid rgba(0,0,0,.1);`;
+  border-bottom: 1px solid rgba(0,0,0,.1);
+  font-family: "Nunito Sans", -apple-system, ".SFNSText-Regular", "San Francisco", BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+`;
 const ComponentTitle = styled.h3`
-  margin: 0 0 8px;`;
+  margin: 20px 0 8px;
+  font-family: "Nunito Sans", -apple-system, ".SFNSText-Regular", "San Francisco", BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  h2 + & {
+    margin-top: 0;
+    padding-top: 0;
+  }
+`;
 const ApiElTitle = styled.h4`
-  margin: 0 0 8px;`;
+  margin: 20px 0 8px;
+  font-family: "Nunito Sans", -apple-system, ".SFNSText-Regular", "San Francisco", BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+`;
 const Text = styled.p`
   margin: 0 0 8px 8px;
   font-size: 14px`;
 
-export const ComponentsApiSection = ({ diffs }) => <Section>
+export const ComponentsApiSection = ({ diffs }) => <React.Fragment>
   <SectionTitle>Changes in the API of components</SectionTitle>
-  <List>
-    { Object.keys(diffs).map((componentName) => <ListItem key={componentName}>
+    { Object.keys(diffs).map((componentName) => <React.Fragment key={componentName}>
       <ComponentTitle>{componentName}</ComponentTitle>
       { Object.keys(diffs[componentName]).map((apiEl, index) => {
         if (apiEl === 'component') {
@@ -41,21 +51,19 @@ export const ComponentsApiSection = ({ diffs }) => <Section>
             type,
             description || '',
           ]);
-          return <List key={index}>
-            <ListItem>
-              <ApiElTitle>{`• ${apiEl} changes`}</ApiElTitle>
-              <Table
-                headers={[
-                  'Name',
-                  'Status',
-                  'Description',
-                ]}
-                rows={ rows }
-              />
-            </ListItem>
-          </List>;
+          const apiElCapitalized = capitalizeFirst(apiEl);
+          return <React.Fragment key={index}>
+            <ApiElTitle>{`${apiElCapitalized} changes`}</ApiElTitle>
+            <Table
+              headers={[
+                apiElCapitalized,
+                'Status',
+                'Description',
+              ]}
+              rows={ rows }
+            />
+          </React.Fragment>;
         }
       })}
-    </ListItem>)}
-  </List>
-</Section>;
+    </React.Fragment>)}
+</React.Fragment>;
