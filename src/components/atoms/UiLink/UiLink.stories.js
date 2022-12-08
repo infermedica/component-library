863c6b3ee1a29e371438f124031e54a379bc13d9
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2022-12-08 13:11:41 +0100

    feat(docs): add stories for link in text

--- src/components/atoms/UiLink/UiLink.stories.js
+++ src/components/atoms/UiLink/UiLink.stories.js
@@ -144,3 +144,34 @@ LinkSecondary.args = { modifiers: [ 'ui-button--theme-secondary' ] };
 export const LinkOnBrand = Template.bind({});
 LinkOnBrand.args = { modifiers: [ 'ui-button--theme-brand' ] };
 LinkOnBrand.parameters = { backgrounds: { default: 'brand' } };
+
+export const LinkInTheLongText = (args) => ({
+  components: {
+    UiLink,
+    UiText,
+  },
+  setup() {
+    return { ...args };
+  },
+  template: `<UiText style="max-width: 600px">
+    Infermedica Component Library is a highly configurable UI components library. To configure components to your needs, you can use a lot of global CSS variables and adjust styles to your requirements. More details you can find in "<UiLink href="http://localhost:6006/iframe.html?path=/docs/getting-started-development-guide-theme-configuration--page">Theme Configuration</UiLink>" 
+    guideline.
+  </UiText>`,
+});
+
+export const LinkInTheLongTextWithIcon = (args) => ({
+  components: {
+    UiLink,
+    UiText,
+    UiIcon,
+  },
+  setup() {
+    return { ...args };
+  },
+  template: `<UiText style="max-width: 600px">
+    Infermedica Component Library is a highly configurable UI components library. To configure components to your needs, you can use a lot of global CSS variables and adjust styles to your requirements. More details you can find in "<UiLink href="http://localhost:6006/iframe.html?path=/docs/getting-started-development-guide-theme-configuration--page">
+      <UiIcon icon="plus-circled-filled" class="ui-link__icon"/>Theme Configuration
+    </UiLink>" 
+    guideline.
+  </UiText>`,
+});
