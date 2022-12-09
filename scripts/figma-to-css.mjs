771457ca2bc415a642f54c6c546a2dd1cc343f0f
Arkadiusz Krzymieński <a.krzymienski@gmail.com>
2022-12-09 08:54:54 +0100

    refactor: some improvements in figma-to-css file

--- scripts/figma-to-css.mjs
+++ scripts/figma-to-css.mjs
@@ -7,23 +7,28 @@ const fileName = fileURLToPath(import.meta.url);
 const figmaToken = process.env.FIGMA_TOKEN;
 const figmaUrl = 'https://api.figma.com/v1';
 const figmaFileId = 'txuY6Y2evaphl43mSvHUQ8';
-const figmaServiceOptions = {
-  method: 'GET',
-  headers: { 'X-Figma-Token': figmaToken },
+
+const saveFile = (name, content) => {
+  try {
+    const comment = '// Auto-generated file by update:css-variables script. Do not edit manually\n\n:root {';
+    writeFileSync(`./src/styles/variables/${name}.scss`, `${comment}${content.join('\n\n')}}`, 'utf-8');
+    console.log(`🚀 ${name} file has been updated!`);
+  } catch (err) {
+    console.log(`⛔️ Something goes wrong and the ${name} file hasn't been updated!`, err);
+  }
 };
-const generatedCodeText = '// Auto-generated file by update:css-bariables script. Do not edit manually';
-const saveFile = (content, name) => {
-  writeFileSync(`./src/styles/variables/${name}`, content, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log(`${name} file has been saved!`);
-  });
-};
-const getFile = async (key) => {
-  const response = await fetch(`${figmaUrl}/files/${key}`, figmaServiceOptions);
-  const data = await response.json();
-  return data;
+const fetchFile = async () => {
+  try {
+    const response = await fetch(`${figmaUrl}/files/${figmaFileId}`, {
+      method: 'GET',
+      headers: { 'X-Figma-Token': figmaToken },
+    });
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(`⛔️ Something goes wrong, ${err}`);
+    return {};
+  }
 };
 const getValueWithUnit = (value, unit = 'px') => {
   if (value?.match(/^0/g)) {
@@ -37,9 +42,8 @@ const getValue = (value, typeOfValue) => (value?.match(new RegExp(typeOfValue, '
   ? `var(--${value})`
   : value);
 const getTokens = ({
-  frames, frameName, listName = 'Lists', nesting = 2, filter = ({ name }) => name?.match(/Name|Value/g), map = ({ characters }) => characters,
+  frames, frameName, listName = 'Lists', nesting = 1, filter = ({ name }) => name?.match(/Name|Value/g), map = ({ characters }) => characters,
 }) => {
-  // Get Lists of Frames With Tokens
   let tokensFrames = frames
     ?.find(({ name }) => (name?.match(new RegExp(frameName, 'g'))))?.children
     ?.find(({ name }) => (name?.match(new RegExp(listName, 'g'))))?.children;
@@ -52,36 +56,17 @@ const getTokens = ({
     ?.map(({ children }) => children
       ?.filter(filter)
       ?.map(map))
-    ?.filter((arr) => arr?.length === 2);
+    ?.filter((arr) => arr?.length === 2)
+    .sort(new Intl.Collator(undefined, {
+      numeric: true,
+      sensitivity: 'base',
+    }).compare);
 };
 
 const figmaToCss = async () => {
-  const { document } = await getFile(figmaFileId);
+  const { document } = await fetchFile();
   const tokensFrames = document?.children
     ?.find(({ name }) => name?.match(/Tokens/g))?.children;
-
-  const colorDecisionsTokens = getTokens({
-    frames: tokensFrames,
-    frameName: 'Color Decisions',
-  });
-  const colorDecisionsCSS = colorDecisionsTokens
-    ?.map(([
-      name,
-      value,
-    ]) => `--${name}: ${getValue(value, 'color')};`)
-    .join('\n');
-
-  const fontTokens = getTokens({
-    frames: tokensFrames,
-    frameName: 'Font',
-  });
-  const fontCSS = fontTokens
-    ?.map(([
-      name,
-      value,
-    ]) => `--${name}: ${value.replace(/\n.+/g, '')};`)
-    .join('\n');
-
   const fontStyleTokens = getTokens({
     frames: tokensFrames,
     frameName: 'Font Style',
@@ -110,14 +95,13 @@ const figmaToCss = async () => {
         case 'Name':
           return frame.characters;
         case 'Frame 1105':
-          if (frame?.children.length === 2) {
+          if (frame?.children.length > 1) {
             return frame.children
               ?.reduce((acc, { children }) => acc.concat(children), [])
               ?.filter(({ characters }) => !characters?.match(/Desktop|Mobile/g))
               ?.map(getFontProperties);
           }
-          return frame?.children
-            ?.map(getFontProperties);
+          return frame?.children?.map(getFontProperties);
         default:
           return [];
       }
@@ -157,7 +141,7 @@ const figmaToCss = async () => {
       fontFamily, fontSize, fontWeight, lineHeight,
     },
   ]) => `--${name}: ${fontWeight} ${fontSize} / ${lineHeight} ${fontFamily};`).join('\n')}
-    \n@media screen and (min-width: 768px) {\n
+    \n@media screen and (min-width: 768px) {
     ${mobileDesktopFontStyleTokens.desktop.map(([
     name,
     {
@@ -167,38 +151,13 @@ const figmaToCss = async () => {
   const letterSpacingCSS = `${mobileDesktopFontStyleTokens.mobile.map(([
     name,
     { letterSpacing },
-  ]) => `--letter-spacing-${name}: ${letterSpacing};`).join('\n')}
+  ]) => `--letter-spacing-${name.replace('font-', '')}: ${letterSpacing};`).join('\n')}
     \n@media screen and (min-width: 768px) {
     ${mobileDesktopFontStyleTokens.desktop.map(([
     name,
     { letterSpacing },
-  ]) => `--letter-spacing-${name}: ${letterSpacing};`).join('\n')}\n}`;
-
-  const spaceTokens = getTokens({
-    frames: tokensFrames,
-    frameName: 'Space',
-    nesting: 1,
-  });
-  const spaceCSS = spaceTokens
-    ?.map(([
-      name,
-      value,
-    ]) => `--${name}: ${value.replace(/\n.+/g, '')};`)
-    .join('\n');
-
-  const borderRadiusTokens = getTokens({
-    frames: tokensFrames,
-    frameName: 'Border Radius',
-    nesting: 1,
-  });
-  const borderRadiusCSS = borderRadiusTokens
-    ?.map(([
-      name,
-      value,
-    ]) => `  --${name}: ${value.replace(/\n.+/g, '')};`)
-    .join('\n');
-
-  const getCSSBoxShadow = ({ characters }) => {
+  ]) => `--letter-spacing-${name.replace('font-', '')}: ${letterSpacing};`).join('\n')}\n}`;
+  const getCSSBoxShadow = (characters) => {
     const isInset = characters.includes('(inset/inner shadow)') ? 'inset ' : '';
     return characters
       ?.split('+')
@@ -215,55 +174,53 @@ const figmaToCss = async () => {
         return `${isInset}${getValueWithUnit(offsetX)} ${getValueWithUnit(offsetY)} ${getValueWithUnit(blurRadius)} ${getValueWithUnit(spreadRadius)} ${getValue(color, 'color')}`;
       }).join(', ');
   };
-  const boxShadowTokens = getTokens({
+  const getCSSVars = (frameName, setValue = (value) => value.replace(/\n.+/g, ''), nesting = 1) => getTokens({
     frames: tokensFrames,
-    frameName: 'Box Shadow',
-    nesting: 1,
-  });
-  const boxShadowCSS = boxShadowTokens
-    ?.map(([
+    frameName,
+    nesting,
+  })?.map((
+    [
       name,
       value,
-    ]) => `--${name}: ${getCSSBoxShadow({ characters: value })};`)
-    .join('\n');
+    ],
+    index,
+    tokens,
+  ) => {
+    const getVariableHead = (varName) => varName.slice(2).split('-').slice(0, 2).join('');
+    const isEmptyLine = index > 0
+      && !(name.includes('space') || name.includes('focus'))
+      && getVariableHead(name) !== getVariableHead(tokens[index - 1][0])
+      ? '\n' : '';
+    return `${isEmptyLine}--${name}: ${setValue(value)};`;
+  }).join('\n');
 
-  const focusTokens = getTokens({
-    frames: tokensFrames,
-    frameName: 'Focus',
-    nesting: 1,
-  });
-  const focusCSS = focusTokens
-    ?.map(([
-      name,
-      value,
-    ]) => `--${name}: ${getCSSBoxShadow({ characters: value })};`)
-    .join('\n');
-
-  const opacityTokens = getTokens({
-    frames: tokensFrames,
-    frameName: 'Opacity',
-    nesting: 1,
-  });
-  const opacityCSS = opacityTokens
-    ?.map(([
-      name,
-      value,
-    ]) => `--${name}: ${value.replace(/\n.+/g, '')};`)
-    .join('\n');
-  const getContent = (properties) => `${generatedCodeText}\n:root {\n${properties.join('\n\n')}\n}`;
-  saveFile(getContent([ colorDecisionsCSS ]), 'colors.scss', false);
-  saveFile(getContent([
-    spaceCSS,
-    borderRadiusCSS,
-    boxShadowCSS,
-    focusCSS,
-    opacityCSS,
-  ]), 'layouts.scss');
-  saveFile(getContent([
-    fontCSS,
+  saveFile('colors', [
+    getCSSVars('Color Decisions', (value) => getValue(value, 'color'), 2),
+    `--color-switch-disabled: var(--color-gray-300);
+    --color-switch-track: var(--color-gray-600);
+    --color-switch-track-hover: var(--color-gray-700);
+    --color-switch-track-active: var(--color-gray-800);
+    --color-switch-track-checked: var(--color-blue-600);
+    --color-switch-track-checked-hover: var(--color-blue-700);
+    --color-switch-track-checked-active: var(--color-blue-800);
+    --color-switch-thumb: var(--color-white);`,
+  ]);
+  saveFile('layouts', [
+    getCSSVars('Space'),
+    getCSSVars('Border Radius'),
+    getCSSVars('Box Shadow', getCSSBoxShadow),
+    getCSSVars('Focus', getCSSBoxShadow),
+    getCSSVars('Opacity'),
+  ]);
+  saveFile('typography', [
+    getCSSVars('Font', (value) => `"${value.slice(1, -1)}", sans-serif`, 1),
+    `& [dir="rtl"] {
+      --font-family-body: "IBM Plex Sans Arabic", sans-serif;
+      --font-family-heading: "IBM Plex Sans Arabic", sans-serif;}`,
+    getCSSVars('Font', (value) => value.replace(/\n.+/g, ''), 2),
     fontStyleCSS,
     letterSpacingCSS,
-  ]), 'typography.scss', false);
+  ]);
 };
 
 export default figmaToCss;
