543cc94c0ffe637aaa989106ee4c2f292cb40828
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2022-12-12 17:43:06 +0100

    fix: lint css logical properties stories

--- docs/contributing-guide/css-logical-properties/css-logical-properties.stories.js
+++ docs/contributing-guide/css-logical-properties/css-logical-properties.stories.js
@@ -1,4 +1,3 @@
-
 import UiButton from '@/components/atoms/UiButton/UiButton.vue';
 import UiText from '@/components/atoms/UiText/UiText.vue';
 import withUseLogicalMixinCode from '../../../.storybook/decorators/withUseLogicalMixinCode';
@@ -71,14 +70,24 @@ export const BorderWidth = (args) => ({
       ...props.value,
     };
   },
-  template: `<UiText class="docs__label">Logical {{property}}</UiText>
-    <UiButton class="docs__border ui-button--outlined" :style="logicalStyle">
-      Label
-    </UiButton>
-    <UiText class="docs__label">Physical {{property}}</UiText>
-    <UiButton class="docs__border ui-button--outlined" :style="physicalStyle">
-      Label
-    </UiButton>`,
+  template: `<UiText class="docs__label">
+  Logical {{ property }}
+  </UiText>
+  <UiButton 
+    class="docs__border ui-button--outlined" 
+    :style="logicalStyle"
+  >
+    Label
+  </UiButton>
+  <UiText class="docs__label">
+    Physical {{ property }}
+  </UiText>
+  <UiButton 
+    class="docs__border ui-button--outlined" 
+    :style="physicalStyle"
+  >
+    Label
+  </UiButton>`,
 });
 BorderWidth.template = {};
 BorderWidth.args = {
