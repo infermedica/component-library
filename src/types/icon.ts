a6cd90100d6cdd13ec85c93832dc585b5335d3b5
Arkadiusz Krzymieński <a.krzymienski@gmail.com>
2023-01-12 15:34:54 +0100

    fix: change type for checkbox value

--- src/types/icon.ts
+++ src/types/icon.ts
@@ -2,7 +2,3 @@ import type iconList from '../components/atoms/UiIcon/icons';
 
 export type IconAsString = typeof iconList[number] | '';
 export type Icon = IconAsString | SVGElement | undefined;
-export interface IconAttrs {
-  icon: Icon;
-  [key: string]: unknown;
-}
