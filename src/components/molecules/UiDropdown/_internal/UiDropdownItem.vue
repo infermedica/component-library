74bf01cffa39690f6128026ed0534a446b28cdd7
Przemek Spaczek <przemyslaw.spaczek@infermedica.com>
2023-01-13 16:13:09 +0100

    feat: helper for css logical properties
    * refactor: split use-logical to smallest parts
    
    * feat: allow to use use-logical to overwrite existing css var

--- src/components/molecules/UiDropdown/_internal/UiDropdownItem.vue
+++ src/components/molecules/UiDropdown/_internal/UiDropdownItem.vue
@@ -101,22 +101,9 @@ const buttonItemAttrs = computed(() => ({
 .ui-dropdown-item {
   $element: dropdown-item;
 
-  --button-padding-block:
-    #{functions.var(
-      $element,
-      padding-block,
-      functions.var($element, padding-block-start, var(--space-8))
-      functions.var($element, padding-block-end, var(--space-8))
-    )};
-  --button-padding-inline:
-    #{functions.var(
-      $element,
-      padding-inline,
-      functions.var($element, padding-inline-start, var(--space-8))
-      functions.var($element, padding-inline-end, var(--space-8))
-    )};
-  --button-border-block-width: 0;
-  --button-border-inline-width: 0;
+  @include mixins.override-logical(button, $element, padding, var(--space-8));
+  @include mixins.override-logical(button, null, border-width, 0);
+
   --button-color: #{functions.var($element, color, var(--color-text-body))};
   --button-hover-color: #{functions.var($element + "-hover", color, var(--color-text-body))};
   --button-active-color: #{functions.var($element + "-active", color, var(--color-text-body))};
