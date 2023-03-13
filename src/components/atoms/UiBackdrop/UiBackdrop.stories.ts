1dde8d3c6edb606050ce93005b28409c48b90645
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2023-03-13 16:28:42 +0100

    docs: stories for UiBackdrop by TypeScript

---
+++ src/components/atoms/UiBackdrop/UiBackdrop.stories.ts
@@ -0,0 +1,19 @@
+import type {
+  Meta,
+  StoryObj,
+} from '@storybook/vue3';
+import UiBackdrop from '@/components/atoms/UiBackdrop/UiBackdrop.vue';
+
+const meta = {
+  title: 'Atoms/Backdrop',
+  component: UiBackdrop,
+  decorators: [ () => ({ template: '<div class="min-h-80"><story/></div>' }) ],
+} satisfies Meta<typeof UiBackdrop>;
+export default meta;
+type Story = StoryObj<typeof UiBackdrop>;
+export const Basic: Story = {
+  render: () => ({
+    components: { UiBackdrop },
+    template: '<UiBackdrop />',
+  }),
+};
