835bd806b68da7bf7704a22903c780d2e061e89c
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2023-03-16 12:16:23 +0100

    docs: remove descriptions; add TODO: Describe

--- src/components/atoms/UiHeading/UiHeading.stories.ts
+++ src/components/atoms/UiHeading/UiHeading.stories.ts
@@ -8,8 +8,13 @@ import { components } from '@storybook/components';
 const meta = {
   title: 'Atoms/Heading',
   component: UiHeading,
-  args: { level: 2 },
+  args: { content: 'How to use it?' },
   argTypes: {
+    content: {
+      description: 'Use this control to set the content.',
+      table: { category: 'stories controls' },
+      control: 'text',
+    },
     level: {
       control: {
         type: 'range',
@@ -17,6 +22,8 @@ const meta = {
         max: 6,
       },
     },
+    tag: { control: { type: 'text' } },
+    default: { control: false },
   },
 } satisfies Meta<typeof UiHeading>;
 export default meta;
@@ -26,33 +33,86 @@ export const Basic: Story = {
   render: (args) => ({
     components: { UiHeading },
     setup() {
-      return { args };
+      const {
+        content, ...rest
+      } = args;
+      return {
+        content,
+        rest,
+      };
     },
-    template: `<UiHeading v-bind="args">
-        Do you have any of the following symptoms?
+    template: `<UiHeading v-bind="rest">
+      {{ content }}
     </UiHeading>`,
   }),
 };
 Basic.args = { level: 2 };
 
-export const WithCustomTag: Story = { ...Basic };
-WithCustomTag.args = { tag: 'span' };
+export const WithLevel: Story = { ...Basic };
+WithLevel.args = { level: 2 };
+WithLevel.parameters = {
+  docs: {
+    source: {
+      code: `<template>
+  <UiHeading level="1">
+    {{ content }}
+  </UiHeading>
+</template>
+
+<script setup lang="ts">
+import { UiHeading } from '@infermedica/component-library';
+</script>`,
+    },
+  },
+};
+
+export const WithTag: Story = { ...Basic };
+WithTag.args = { tag: 'span' };
+WithTag.parameters = {
+  docs: {
+    source: {
+      code: `<template>
+  <UiHeading tag="span">
+    {{ content }}
+  </UiHeading>
+</template>
+
+<script setup lang="ts">
+import { UiHeading } from '@infermedica/component-library';
+</script>`,
+    },
+  },
+};
 
 export const AllVariants: Story = {
   render: (args) => ({
     components: { UiHeading },
+    setup() {
+      const {
+        content, ...rest
+      } = args;
+
+      return { content };
+    },
     template: `<template v-for="level in 5">
       <UiHeading :level="level">
-        Do you have any of the following symptoms?
+        {{ content }}
       </UiHeading>
     </template>`,
   }),
+};
+AllVariants.parameters = {
+  chromatic: { disableSnapshot: false },
+  docs: { source: { code: null } },
 };
 
 export const Secondary: Story = { ...AllVariants };
 Secondary.decorators = [ () => ({ template: '<div class="ui-heading--theme-secondary"><story/></div>' }) ];
 
-export const OnBrand: Story = { ...AllVariants };
-OnBrand.parameters = { backgrounds: { default: 'brand' } };
-OnBrand.decorators = [ () => ({ template: '<div class="ui-heading--theme-brand"><story/></div>' }) ];
+export const Brand: Story = { ...AllVariants };
+Brand.parameters = {
+  ...AllVariants.parameters,
+  backgrounds: { default: 'brand' },
+};
+Brand.decorators = [ () => ({ template: '<div class="ui-heading--theme-brand"><story/></div>' }) ];
 
