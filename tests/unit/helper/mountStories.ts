5a389fc49425561f24068c2bbb2fb6ba10d34b40
Arkadiusz Krzymieński <a.krzymienski@gmail.com>
2023-03-24 16:48:01 +0100

    test: small improvements for mountStories helper

--- tests/unit/helper/mountStories.ts
+++ tests/unit/helper/mountStories.ts
@@ -1,34 +1,47 @@
 /* eslint-disable import/prefer-default-export */
+import { mount } from '@vue/test-utils';
+import type { VueWrapper } from '@vue/test-utils';
+import type { Component } from 'vue';
 import type {
   Meta,
-  StoryContext,
   StoryObj,
 } from '@storybook/vue3';
-import type { ComponentPublicInstance } from 'vue';
 
-export const mountStories = <T extends {default: Meta} & Record<string, StoryObj>>(
-  storiesImport: T,
+type ReturnTypeMountedStory<T> = () => {
+  component: VueWrapper,
+  args: T,
+};
+
+export const mountStories = <StoryImport extends {default: Meta} & Record<string, StoryObj>>(
+  component: Component, storiesImport: StoryImport, setWrapper: (story: VueWrapper) => void,
 ) => {
   const {
     default: meta, ...stories
   } = storiesImport;
-  return Object.entries(stories).reduce(
-    (storiesMap, [
-      key,
-      story,
-    ]: [
-      string,
-      Required<StoryObj>
-    ]) => ({
+  return Object.entries(stories).reduce((storiesMap, [
+    key,
+    story,
+  ]: [
+    string,
+    Required<StoryObj>
+  ]) => {
+    const args: typeof story['args'] = {
+      ...(meta?.args || {}),
+      ...(story?.args || {}),
+    };
+    const mountedStory: VueWrapper = mount(story.render(
+      args,
+      meta as any,
+    ) as any, { props: args });
+    return {
       ...storiesMap,
-      [key]: () => story.render(
-        {
-          ...(meta?.args || {}),
-          ...(story?.args || {}),
-        },
-        meta as StoryContext,
-      ),
-    }),
-    {} as Record<keyof typeof stories, () => ComponentPublicInstance>,
-  );
+      [key]: () => {
+        setWrapper(mountedStory);
+        return {
+          component: mountedStory.findComponent(component),
+          args,
+        };
+      },
+    };
+  }, {} as Record<keyof typeof stories, ReturnTypeMountedStory<typeof stories[keyof typeof stories]['args']>>);
 };
