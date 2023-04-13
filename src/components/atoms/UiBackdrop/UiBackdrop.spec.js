53621434bd5e5a1949ee2d4bd059cf61b5621769
Arkadiusz Krzymieński <a.krzymienski@gmail.com>
2023-04-13 09:59:36 +0200

    test: rewrite UiBackdrop tests by TypeScript

--- src/components/atoms/UiBackdrop/UiBackdrop.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import UiBackdrop from './UiBackdrop.vue';
-
-describe('UiBackdrop.vue', () => {
-  test('renders a component', () => {
-    const wrapper = mount(UiBackdrop);
-    expect(wrapper.classes('ui-backdrop')).toBe(true);
-  });
-});
---
+++ src/components/atoms/UiBackdrop/UiBackdrop.spec.ts
@@ -0,0 +1,19 @@
+import { mountStories } from '@tests/unit/helpers';
+import { UiBackdrop } from '@index';
+import type { VueWrapper } from '@vue/test-utils';
+import * as backdropStoriesImports from './UiBackdrop.stories';
+
+describe('UiBackdrop.vue', () => {
+  let wrapper: VueWrapper;
+  const setWrapper = (storyWrapper: VueWrapper) => {
+    wrapper = storyWrapper;
+  };
+  const { Basic } = mountStories(UiBackdrop, backdropStoriesImports, setWrapper);
+  afterEach(() => {
+    wrapper.unmount();
+  });
+  test('renders a component', () => {
+    const { component } = Basic();
+    expect(component.classes('ui-backdrop')).toBe(true);
+  });
+});
