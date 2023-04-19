03fd4b7761106ab9f3e3f89a011feb60228d2f6b
Arkadiusz Krzymieński <a.krzymienski@gmail.com>
2023-04-19 15:23:48 +0200

    docs: add Storybook interactions tests

--- src/components/atoms/UiRange/UiRange.stories.ts
+++ src/components/atoms/UiRange/UiRange.stories.ts
@@ -1,3 +1,4 @@
+import { expect } from '@storybook/jest';
 import type {
   Meta,
   StoryObj,
@@ -5,30 +6,40 @@ import type {
 import {
   ref,
   provide,
-  inject
+  inject,
 } from 'vue';
-import deepmerge from "deepmerge";
+import deepmerge from 'deepmerge';
 import {
   UiRange,
-  UiHeading
-} from '@/../index';
+  UiHeading,
+} from '@index';
+import type { RangeProps } from '@index';
 import { withVariants } from '@sb/decorators';
 import {
   useArgTypes,
-  inputEvents
-} from '@sb/helpers'
+  inputEvents,
+} from '@sb/helpers';
+import {
+  userEvent,
+  within,
+} from '@storybook/testing-library';
+
+type RangeArgsType = RangeProps & {
+  ariaLabel?: string
+}
+type RangeMetaType = Meta<RangeArgsType>;
+type RangeStoryType = StoryObj<RangeArgsType>;
 
 const { argTypes } = useArgTypes(deepmerge(
   UiRange,
-  inputEvents
+  inputEvents,
 ));
-
 const meta = {
   title: 'Atoms/Range',
   component: UiRange,
   args: {
     ariaLabel: 'patient age',
-    modelValue: 50,
+    modelValue: 70,
     min: 18,
     max: 122,
     step: 1,
@@ -42,34 +53,33 @@ const meta = {
       description: 'Use this control to set aria-label attribute.',
       control: 'text',
       table: { category: 'html attributes' },
-    }
+    },
   },
   parameters: {
     chromatic: {
       disableSnapshot: false,
-      viewports: [320, 1200],
+      viewports: [
+        320,
+        1200,
+      ],
     },
-    docs: {
-      source: {
-        code: null
-      }
-    }
-  }
-} satisfies Meta<typeof UiRange>;
+    docs: { source: { code: null } },
+  },
+} satisfies RangeMetaType;
 export default meta;
-type Story = StoryObj<typeof UiRange>;
-export const Basic: Story = {
+
+export const Basic: RangeStoryType = {
   render: () => ({
     inheritAttrs: false,
     components: { UiRange },
-    setup( props, { attrs } ) {
+    setup(props, { attrs }) {
       const {
         modelValue,
         ...args
       } = attrs;
       const value = inject('value') || ref(modelValue);
       return {
-        args: args,
+        args,
         value,
       };
     },
@@ -111,69 +121,97 @@ const headingValueAttrs = {
   "data-testid": "value-heading"
 };
 </script>`,
-    }
-  }
-}
+    },
+  },
+};
+Basic.play = async ({
+  canvasElement, step, args: { modelValue = 50 },
+}) => {
+  const [
+    decrementBtn,
+    incrementBtn,
+  ] = [ ...canvasElement.querySelectorAll('button') ];
+  const getCurrentValue = () => within(canvasElement).getByTestId('value-heading').innerText;
+  await step('increment button increase value', async () => {
+    await userEvent.click(incrementBtn);
+    expect(getCurrentValue()).toBe(`${modelValue + 1}`);
+  });
+  await step('decrement button decrease value', async () => {
+    await userEvent.click(decrementBtn);
+    expect(getCurrentValue()).toBe(`${modelValue}`);
+  });
+};
 
-export const PseudoClass: Story = {
-  ...Basic,
-}
+export const PseudoClass: RangeStoryType = { ...Basic };
 PseudoClass.decorators = [
   withVariants,
   () => ({
     inheritAttrs: false,
-    setup( props, { attrs }) {
+    setup(props, { attrs }) {
       const { modelValue } = attrs;
       const value = ref(modelValue);
       provide('value', value);
     },
-    template: `<story />`
-  })
-]
+    template: '<story />',
+  }),
+];
 PseudoClass.parameters = {
   variants: [
-    { label:'default' },
-    ...['hover', 'focus', 'active'].map((variant) => ({
+    { label: 'default' },
+    ...[
+      'hover',
+      'focus',
+      'active',
+    ].map((variant) => ({
       label: `${variant}`,
-      inputAttrs: {
-        class: `pseudo-${variant}`,
-      },
+      inputAttrs: { class: `pseudo-${variant}` },
     })),
-  ]
-}
+  ],
+};
+PseudoClass.play = undefined;
 
-export const Min:Story = {
-  ...Basic
-}
-Min.args = {
-  modelValue: meta.args.min,
-}
-Min.parameters = {
-  docs: {
-    source: {
-      code: null
-    }
-  }
-}
+export const Min: RangeStoryType = { ...Basic };
+Min.args = { modelValue: meta.args.min };
+Min.parameters = { docs: { source: { code: null } } };
+Min.play = async ({
+  canvasElement, step, args: { min },
+}) => {
+  const decrementBtn = canvasElement.querySelector('.ui-number-stepper__decrement') as HTMLButtonElement;
+  const getCurrentValue = () => within(canvasElement).getByTestId('value-heading').innerText;
+  await step('decrement button is disabled', async () => {
+    expect(decrementBtn.className).toContain('ui-button--is-disabled');
+  });
+  await step('decrement button doesn\'t change value', async () => {
+    await userEvent.click(decrementBtn);
+    expect(getCurrentValue()).toBe(`${min}`);
+  });
+};
 
-export const Max:Story = {
-  ...Basic
-}
-Max.args = {
-  modelValue: meta.args.max,
-}
-Max.parameters = {
-  ...Min.parameters,
-}
+export const Max: RangeStoryType = { ...Basic };
+Max.args = { modelValue: meta.args.max };
+Max.parameters = { ...Min.parameters };
+Max.play = async ({
+  canvasElement, step, args: { max },
+}) => {
+  const incrementBtn = canvasElement.querySelector('.ui-number-stepper__increment') as HTMLButtonElement;
+  const getCurrentValue = () => within(canvasElement).getByTestId('value-heading').innerText;
+  await step('increment button is disabled', async () => {
+    expect(incrementBtn.className).toContain('ui-button--is-disabled');
+  });
+  await step('increment button doesn\'t change value', async () => {
+    await userEvent.click(incrementBtn);
+    expect(getCurrentValue()).toBe(`${max}`);
+  });
+};
 
-export const WithValueSlot: Story = {
+export const WithValueSlot: RangeStoryType = {
   render: () => ({
     inheritAttrs: false,
     components: {
       UiRange,
-      UiHeading
+      UiHeading,
     },
-    setup( props, { attrs } ) {
+    setup(props, { attrs }) {
       const {
         modelValue,
         ...args
@@ -202,7 +240,7 @@ export const WithValueSlot: Story = {
       </template>
     </UiRange>`,
   }),
-}
+};
 WithValueSlot.parameters = {
   docs: {
     source: {
@@ -230,8 +268,8 @@ WithValueSlot.parameters = {
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { 
-  UiRange, 
+import {
+  UiRange,
   UiHeading
 } from '@infermedica/component-library';
 
@@ -239,17 +277,17 @@ const ariaLabel = 'patient age';
 const modelValue = ref('');
 const min = 0;
 const max = 1;
-const step = 1; 
-</script>`
-    }
-  }
-}
+const step = 1;
+</script>`,
+    },
+  },
+};
 
-export const WithRangeSlot: Story = {
+export const WithRangeSlot: RangeStoryType = {
   render: () => ({
     inheritAttrs: false,
     components: { UiRange },
-    setup( props, { attrs } ) {
+    setup(props, { attrs }) {
       const {
         modelValue,
         ...args
@@ -288,7 +326,7 @@ export const WithRangeSlot: Story = {
     </template>
     </UiRange>`,
   }),
-}
+};
 WithRangeSlot.parameters = {
   docs: {
     source: {
@@ -328,8 +366,8 @@ WithRangeSlot.parameters = {
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { 
-  UiRange, 
+import {
+  UiRange,
   UiHeading
 } from '@infermedica/component-library';
 
@@ -338,7 +376,7 @@ const modelValue = ref('');
 const min = 0;
 const max = 1;
 const step = 1;
-</script>`
-    }
-  }
-}
+</script>`,
+    },
+  },
+};
