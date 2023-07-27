892d2a38e64fd5f44842f29b5d7c890a0f610e42
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2023-07-27 12:38:54 +0200

    refactor!: stepper without mobile layout

--- src/components/molecules/UiStepper/UiStepper.stories.js
+++ src/components/molecules/UiStepper/UiStepper.stories.js
@@ -43,30 +43,11 @@ export default {
   args: {
     steps,
     currentStep: steps.at(2).label,
-    progressAttrs: { id: 'progress' },
   },
   argTypes: {
     currentStep: {
       control: 'select',
       options: steps.map((step) => step.label),
-    },
-    progressAttrs: { table: { subcategory: 'Attrs props' } },
-  },
-  decorators: [ () => ({
-    template: `<div class="desktop:max-w-35">
-      <story />
-    </div>`,
-  }) ],
-  parameters: {
-    cssProperties: {
-      '--stepper-padding-block':
-        'var(--stepper-padding-block-start, 0) var(--stepper-padding-block-end, 0)',
-      '--stepper-padding-inline':
-        'var(--stepper-padding-inline-start, 0) var(--stepper-padding-inline-end, 0)',
-      '--stepper-background': 'var(--color-background-subtle)',
-      '--stepper-desktop-background': 'transparent',
-      '--stepper-progress-width': '6rem',
-      '--stepper-tablet-progress-width': '11.25rem',
     },
   },
 };
@@ -82,42 +63,6 @@ export const WithCurrentStep = {
       :current-step="currentStep"
       :progress-attrs="progressAttrs"
     />`,
-  }),
-};
-
-export const WithMobileSlot = {
-  render: (args) => ({
-    components: {
-      UiStepper,
-      UiText,
-      UiProgress,
-    },
-    setup() {
-      return { ...args };
-    },
-    template: `<UiStepper
-      :steps="steps"
-      :current-step="currentStep"
-      :progress-attrs="progressAttrs"
-    >
-      <template #mobile="{
-        currentStepDisplayText,
-        progressAttrs,
-      }">
-        <div class="ui-stepper__mobile">
-          <UiText
-            tag="span"
-            class="ui-text--body-2-comfortable ui-stepper__current-step"
-          >
-            {{ currentStepDisplayText }}
-          </UiText>
-          <UiProgress
-            v-bind="progressAttrs"
-            class="ui-stepper__progress"
-          />
-        </div>
-      </template>
-    </UiStepper>`,
   }),
 };
 
@@ -142,68 +87,6 @@ export const WithCurrentStepSlot = {
         >
           {{ currentStepDisplayText }}
         </UiText>
-      </template>
-    </UiStepper>`,
-  }),
-};
-
-export const WithProgressSlot = {
-  render: (args) => ({
-    components: {
-      UiStepper,
-      UiProgress,
-    },
-    setup() {
-      return { ...args };
-    },
-    template: `<UiStepper
-      :steps="steps"
-      :current-step="currentStep"
-      :progress-attrs="progressAttrs"
-    >
-      <template #progress="{ progressAttrs }">
-        <UiProgress
-          v-bind="progressAttrs"
-          class="ui-stepper__progress"
-        />
-      </template>
-    </UiStepper>`,
-  }),
-};
-
-export const WithDesktopSlot = {
-  render: (args) => ({
-    components: {
-      UiStepper,
-      UiStepperStep,
-      UiList,
-    },
-    setup() {
-      return { ...args };
-    },
-    template: `<UiStepper
-      :steps="steps"
-      :current-step="currentStep"
-      :progress-attrs="progressAttrs"
-    >
-      <template #desktop="{
-        steps,
-        currentStep,
-        indexOfActiveStep,
-        stepperStepAttrs,
-      }">
-        <UiList class="ui-stepper__desktop">
-          <template 
-            v-for="(step, index) in steps"
-            :key="index"
-          >
-            <UiStepperStep
-              :index="index"
-              :index-of-active-step="indexOfActiveStep"
-              v-bind="stepperStepAttrs(step)"
-            />
-          </template>
-        </UiList>
       </template>
     </UiStepper>`,
   }),
