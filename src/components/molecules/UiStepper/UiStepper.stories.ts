22c18071a97ed610072e1472e55c89e1ec8891cc
sjarczak <sonia.jarczak@infermedica.com>
2023-07-27 09:11:31 +0200

    docs: rewrite stories with new decorator

--- src/components/molecules/UiStepper/UiStepper.stories.ts
+++ src/components/molecules/UiStepper/UiStepper.stories.ts
@@ -1,10 +1,9 @@
 import {
   computed,
-  ref,
   provide,
   inject,
-  toRefs,
 } from 'vue';
+import type { WritableComputedRef } from 'vue';
 import type {
   Meta,
   StoryObj,
@@ -21,7 +20,6 @@ import type { StepperProps } from '@index';
 import UiStepperStep from './_internal/UiStepperStep.vue';
 import type { StepperStepAttrsProps } from './_internal/UiStepperStep.vue';
 
-// TODO: figure out how to don't reload page after click changing step
 const stepperSteps: StepperStepAttrsProps[] = [
   {
     label: 'Introduction',
@@ -58,6 +56,7 @@ const stepperSteps: StepperStepAttrsProps[] = [
 type StepperArgsType = StepperProps;
 type StepperMetaType = Meta<StepperArgsType>;
 type StepperStoryType = StoryObj<StepperArgsType>;
+type StepperSteps = { steps: StepperStepAttrsProps[] };
 
 const { argTypes } = useArgTypes(UiStepper);
 const meta = {
@@ -88,20 +87,21 @@ const meta = {
     return {
       components: { story },
       setup(props, { attrs }) {
-        const currentStep = computed({
+        const currentStepAttrs = computed({
           get: () => (attrs.currentStep),
           set: (newValue) => {
             updateArgs({ currentStep: newValue });
           },
         });
-        provide('currentStep', currentStep);
+        provide('currentStep', currentStepAttrs);
         const args = computed(() => (Object.keys(attrs)
           .reduce(
             (object, key) => {
+              const storyAttrs: Partial<StepperSteps> | StepperSteps = { ...object };
               if (key !== 'currentStep') {
-                object[key] = attrs[key];
+                (storyAttrs as StepperStepAttrsProps)[key] = attrs[key];
               }
-              return object;
+              return storyAttrs;
             },
             {},
           )));
@@ -131,17 +131,16 @@ export const Basic: StepperStoryType = {
     inheritAttrs: false,
     components: { UiStepper },
     setup(props, { attrs }) {
-      const { steps } = toRefs(attrs);
+      const { steps } = attrs;
       const currentStep = inject('currentStep');
       const args = computed(() => (attrs));
-      const handleStepClick = (step, event) => {
+      const handleStepClick = (step: StepperStepAttrsProps, event: Event) => {
         event.preventDefault();
-        currentStep.value = step.label;
+        (currentStep as WritableComputedRef<unknown>).value = step.label;
       };
-      const stepsToRender = computed(() => ((steps.value as StepperStepAttrsProps[]).map((step) => ({
+      const stepsToRender = computed(() => ((steps as StepperStepAttrsProps[]).map((step) => ({
         ...step,
-        data: 'data',
-        onClick: handleStepClick.bind(event, step),
+        onClick: handleStepClick.bind(this, step),
       }))));
 
       return {
@@ -158,262 +157,292 @@ export const Basic: StepperStoryType = {
   }),
 };
 
-// export const WithMobileSlot: StepperStoryType = {
-//   render: () => ({
-//     components: {
-//       UiStepper,
-//       UiText,
-//       UiProgress,
-//     },
-//     setup(props, { attrs }) {
-//       const {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args
-//       } = attrs;
-//
-//       return {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args,
-//       };
-//     },
-//     template: `<UiStepper
-//       :steps="steps"
-//       :current-step="currentStep"
-//       :progress-attrs="progressAttrs"
-//     >
-//       <template #mobile="{
-//         currentStepDisplayText,
-//         progressAttrs,
-//       }">
-//         <div class="ui-stepper__mobile">
-//           <UiText
-//             tag="span"
-//             class="ui-text--body-2-comfortable ui-stepper__current-step"
-//           >
-//             {{ currentStepDisplayText }}
-//           </UiText>
-//           <UiProgress
-//             v-bind="progressAttrs"
-//             class="ui-stepper__progress"
-//           />
-//         </div>
-//       </template>
-//     </UiStepper>`,
-//   }),
-// };
-//
-// export const WithCurrentStepSlot: StepperStoryType = {
-//   render: () => ({
-//     components: {
-//       UiStepper,
-//       UiText,
-//     },
-//     setup(props, { attrs }) {
-//       const {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args
-//       } = attrs;
-//
-//       return {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args,
-//       };
-//     },
-//     template: `<UiStepper
-//     :steps="steps"
-//     :current-step="currentStep"
-//     :progress-attrs="progressAttrs"
-//   >
-//     <template #current-step="{ currentStepDisplayText }">
-//       <UiText
-//         tag="span"
-//         class="ui-text--body-2-comfortable ui-stepper__current-step"
-//       >
-//         {{ currentStepDisplayText }}
-//       </UiText>
-//     </template>
-//   </UiStepper>`,
-//   }),
-// };
-//
-// export const WithProgressSlot: StepperStoryType = {
-//   render: () => ({
-//     components: {
-//       UiStepper,
-//       UiProgress,
-//     },
-//     setup(props, { attrs }) {
-//       const {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args
-//       } = attrs;
-//
-//       return {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args,
-//       };
-//     },
-//     template: `<UiStepper
-//       :steps="steps"
-//       :current-step="currentStep"
-//       :progress-attrs="progressAttrs"
-//     >
-//       <template #progress="{ progressAttrs }">
-//         <UiProgress
-//           v-bind="progressAttrs"
-//           class="ui-stepper__progress"
-//         />
-//       </template>
-//     </UiStepper>`,
-//   }),
-// };
-//
-// export const WithDesktopSlot: StepperStoryType = {
-//   render: () => ({
-//     components: {
-//       UiStepper,
-//       UiStepperStep,
-//       UiList,
-//     },
-//     setup(props, { attrs }) {
-//       const {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args
-//       } = attrs;
-//
-//       return {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args,
-//       };
-//     },
-//     template: `<UiStepper
-//       :steps="steps"
-//       :current-step="currentStep"
-//       :progress-attrs="progressAttrs"
-//     >
-//       <template #desktop="{
-//         steps,
-//         currentStep,
-//         activeStepIndex,
-//         stepperStepAttrs,
-//       }">
-//         <UiList class="ui-stepper__desktop">
-//           <template
-//             v-for="(step, index) in steps"
-//             :key="index"
-//           >
-//             <UiStepperStep
-//               :index="index"
-//               :active-step-index="activeStepIndex"
-//               v-bind="stepperStepAttrs(step)"
-//             />
-//           </template>
-//         </UiList>
-//       </template>
-//     </UiStepper>`,
-//   }),
-// };
-//
-// export const WithItemsSlot: StepperStoryType = {
-//   render: () => ({
-//     components: {
-//       UiStepper,
-//       UiStepperStep,
-//     },
-//     setup(props, { attrs }) {
-//       const {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args
-//       } = attrs;
-//
-//       return {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args,
-//       };
-//     },
-//     template: `<UiStepper
-//       :steps="steps"
-//       :current-step="currentStep"
-//       :progress-attrs="progressAttrs"
-//     >
-//      <template #items="{
-//        steps,
-//        activeStepIndex,
-//        stepperStepAttrs
-//      }">
-//        <template
-//          v-for="(step, index) in steps"
-//          :key="index"
-//        >
-//          <UiStepperStep
-//            :index="index"
-//            :active-step-index="activeStepIndex"
-//            v-bind="stepperStepAttrs(step)"
-//          />
-//        </template>
-//      </template>
-//     </UiStepper>`,
-//   }),
-// };
-//
-// export const WithItemSlot: StepperStoryType = {
-//   render: () => ({
-//     components: {
-//       UiStepper,
-//       UiStepperStep,
-//     },
-//     setup(props, { attrs }) {
-//       const {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args
-//       } = attrs;
-//
-//       return {
-//         steps,
-//         currentStep,
-//         progressAttrs,
-//         ...args,
-//       };
-//     },
-//     template: `<UiStepper
-//       :steps="steps"
-//       :current-step="currentStep"
-//       :progress-attrs="progressAttrs"
-//     >
-//       <template #item="{
-//         step,
-//         index,
-//         activeStepIndex,
-//         stepperStepAttrs,
-//       }">
-//         <UiStepperStep
-//           :index="index"
-//           :active-step-index="activeStepIndex"
-//           v-bind="stepperStepAttrs(step)"
-//         />
-//       </template>
-//     </UiStepper>`,
-//   }),
-// };
+export const WithMobileSlot: StepperStoryType = {
+  render: () => ({
+    inheritAttrs: false,
+    components: {
+      UiStepper,
+      UiText,
+      UiProgress,
+    },
+    setup(props, { attrs }) {
+      const { steps } = attrs;
+      const currentStep = inject('currentStep');
+      const args = computed(() => (attrs));
+      const handleStepClick = (step: StepperStepAttrsProps, event: Event) => {
+        event.preventDefault();
+        (currentStep as WritableComputedRef<unknown>).value = step.label;
+      };
+      const stepsToRender = computed(() => ((steps as StepperStepAttrsProps[]).map((step) => ({
+        ...step,
+        onClick: handleStepClick.bind(this, step),
+      }))));
+
+      return {
+        currentStep,
+        stepsToRender,
+        args,
+      };
+    },
+    template: `<UiStepper
+      v-bind="args"
+      :current-step="currentStep"
+      :steps="stepsToRender"
+    >
+      <template #mobile="{
+        currentStepDisplayText,
+        progressAttrs,
+      }">
+        <div class="ui-stepper__mobile">
+          <UiText
+            tag="span"
+            class="ui-text--body-2-comfortable ui-stepper__current-step"
+          >
+            {{ currentStepDisplayText }}
+          </UiText>
+          <UiProgress
+            v-bind="progressAttrs"
+            class="ui-stepper__progress"
+          />
+        </div>
+      </template>
+    </UiStepper>`,
+  }),
+};
+
+export const WithCurrentStepSlot: StepperStoryType = {
+  render: () => ({
+    inheritAttrs: false,
+    components: {
+      UiStepper,
+      UiText,
+    },
+    setup(props, { attrs }) {
+      const { steps } = attrs;
+      const currentStep = inject('currentStep');
+      const args = computed(() => (attrs));
+      const handleStepClick = (step: StepperStepAttrsProps, event: Event) => {
+        event.preventDefault();
+        (currentStep as WritableComputedRef<unknown>).value = step.label;
+      };
+      const stepsToRender = computed(() => ((steps as StepperStepAttrsProps[]).map((step) => ({
+        ...step,
+        onClick: handleStepClick.bind(this, step),
+      }))));
+
+      return {
+        currentStep,
+        stepsToRender,
+        args,
+      };
+    },
+    template: `<UiStepper
+      v-bind="args"
+      :current-step="currentStep"
+      :steps="stepsToRender"
+    >
+      <template #current-step="{ currentStepDisplayText }">
+        <UiText
+          tag="span"
+          class="ui-text--body-2-comfortable ui-stepper__current-step"
+        >
+          {{ currentStepDisplayText }}
+        </UiText>
+      </template>
+    </UiStepper>`,
+  }),
+};
+
+export const WithProgressSlot: StepperStoryType = {
+  render: () => ({
+    inheritAttrs: false,
+    components: {
+      UiStepper,
+      UiProgress,
+    },
+    setup(props, { attrs }) {
+      const { steps } = attrs;
+      const currentStep = inject('currentStep');
+      const args = computed(() => (attrs));
+      const handleStepClick = (step: StepperStepAttrsProps, event: Event) => {
+        event.preventDefault();
+        (currentStep as WritableComputedRef<unknown>).value = step.label;
+      };
+      const stepsToRender = computed(() => ((steps as StepperStepAttrsProps[]).map((step) => ({
+        ...step,
+        onClick: handleStepClick.bind(this, step),
+      }))));
+
+      return {
+        currentStep,
+        stepsToRender,
+        args,
+      };
+    },
+    template: `<UiStepper
+      v-bind="args"
+      :current-step="currentStep"
+      :steps="stepsToRender"
+    >
+      <template #progress="{ progressAttrs }">
+        <UiProgress
+          v-bind="progressAttrs"
+          class="ui-stepper__progress"
+        />
+      </template>
+    </UiStepper>`,
+  }),
+};
+
+export const WithDesktopSlot: StepperStoryType = {
+  render: () => ({
+    inheritAttrs: false,
+    components: {
+      UiStepper,
+      UiStepperStep,
+      UiList,
+    },
+    setup(props, { attrs }) {
+      const { steps } = attrs;
+      const currentStep = inject('currentStep');
+      const args = computed(() => (attrs));
+      const handleStepClick = (step: StepperStepAttrsProps, event: Event) => {
+        event.preventDefault();
+        (currentStep as WritableComputedRef<unknown>).value = step.label;
+      };
+      const stepsToRender = computed(() => ((steps as StepperStepAttrsProps[]).map((step) => ({
+        ...step,
+        onClick: handleStepClick.bind(this, step),
+      }))));
+
+      return {
+        currentStep,
+        stepsToRender,
+        args,
+      };
+    },
+    template: `<UiStepper
+      v-bind="args"
+      :current-step="currentStep"
+      :steps="stepsToRender"
+    >
+      <template #desktop="{
+        steps,
+        currentStep,
+        activeStepIndex,
+        stepperStepAttrs,
+      }">
+        <UiList class="ui-stepper__desktop">
+          <template
+            v-for="(step, index) in steps"
+            :key="index"
+          >
+            <UiStepperStep
+              :index="index"
+              :active-step-index="activeStepIndex"
+              v-bind="stepperStepAttrs(step)"
+            />
+          </template>
+        </UiList>
+      </template>
+    </UiStepper>`,
+  }),
+};
+
+export const WithItemsSlot: StepperStoryType = {
+  render: () => ({
+    inheritAttrs: false,
+    components: {
+      UiStepper,
+      UiStepperStep,
+    },
+    setup(props, { attrs }) {
+      const { steps } = attrs;
+      const currentStep = inject('currentStep');
+      const args = computed(() => (attrs));
+      const handleStepClick = (step: StepperStepAttrsProps, event: Event) => {
+        event.preventDefault();
+        (currentStep as WritableComputedRef<unknown>).value = step.label;
+      };
+      const stepsToRender = computed(() => ((steps as StepperStepAttrsProps[]).map((step) => ({
+        ...step,
+        onClick: handleStepClick.bind(this, step),
+      }))));
+
+      return {
+        currentStep,
+        stepsToRender,
+        args,
+      };
+    },
+    template: `<UiStepper
+      v-bind="args"
+      :current-step="currentStep"
+      :steps="stepsToRender"
+    >
+     <template #items="{
+       steps,
+       activeStepIndex,
+       stepperStepAttrs
+     }">
+       <template
+         v-for="(step, index) in steps"
+         :key="index"
+       >
+         <UiStepperStep
+           :index="index"
+           :active-step-index="activeStepIndex"
+           v-bind="stepperStepAttrs(step)"
+         />
+       </template>
+     </template>
+    </UiStepper>`,
+  }),
+};
+
+export const WithItemSlot: StepperStoryType = {
+  render: () => ({
+    inheritAttrs: false,
+    components: {
+      UiStepper,
+      UiStepperStep,
+    },
+    setup(props, { attrs }) {
+      const { steps } = attrs;
+      const currentStep = inject('currentStep');
+      const args = computed(() => (attrs));
+      const handleStepClick = (step: StepperStepAttrsProps, event: Event) => {
+        event.preventDefault();
+        (currentStep as WritableComputedRef<unknown>).value = step.label;
+      };
+      const stepsToRender = computed(() => ((steps as StepperStepAttrsProps[]).map((step) => ({
+        ...step,
+        onClick: handleStepClick.bind(this, step),
+      }))));
+
+      return {
+        currentStep,
+        stepsToRender,
+        args,
+      };
+    },
+    template: `<UiStepper
+      v-bind="args"
+      :current-step="currentStep"
+      :steps="stepsToRender"
+    >
+      <template #item="{
+        step,
+        index,
+        activeStepIndex,
+        stepperStepAttrs,
+      }">
+        <UiStepperStep
+          :index="index"
+          :active-step-index="activeStepIndex"
+          v-bind="stepperStepAttrs(step)"
+        />
+      </template>
+    </UiStepper>`,
+  }),
+};
