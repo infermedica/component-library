e37ce85e0cd6c031a93dba782ea3ea20cef710af
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2023-09-18 13:10:50 +0200

    feat: update check and radio stories

--- src/components/atoms/UiCheckbox/UiCheckbox.stories.ts
+++ src/components/atoms/UiCheckbox/UiCheckbox.stories.ts
@@ -4,8 +4,12 @@ import type {
 } from '@storybook/vue3';
 import { UiCheckbox } from '@index';
 import type { CheckboxProps } from '@index';
-import { withVModel } from '@sb/decorators';
+import {
+  withVariants,
+  withVModel,
+} from '@sb/decorators';
 import { getArgTypes } from '@sb/helpers';
+import { content } from '@sb/helpers/argTypes/index';
 import {
   BasicStories,
   BasicStoriesSource,
@@ -28,19 +32,20 @@ const meta = {
   title: 'Atoms/Checkbox',
   component: UiCheckbox,
   args: {
-    ...args,
-    modelValue: false,
     content: 'I read and accept Terms of Service and Privacy Policy.',
     class: [],
+    modelValue: false,
     value: '',
     id: '',
     disabled: false,
     inputAttrs: { 'data-testid': 'input-element' },
     iconCheckmarkAttrs: { 'data-testid': 'icon-checkmark' },
     textLabelAttrs: { 'data-testid': 'text-label' },
+    ...args,
   },
   argTypes: {
     ...argTypes,
+    content,
     modelValue: { control: 'boolean' },
   },
   parameters: { chromatic: { disableSnapshot: false } },
@@ -52,4 +57,52 @@ Basic.decorators = [ withVModel ];
 Basic.parameters = {
   docs: { source: { code: BasicStoriesSource } },
   chromatic: { disableSnapshot: true },
+};
+
+export const Unchecked: StoryObj = { render: () => (BasicStories) };
+Unchecked.decorators = [ withVariants ];
+Unchecked.parameters = {
+  variants: [
+    {
+      label: 'default',
+      modelValue: false,
+    },
+    ...[
+      'hover',
+      'active',
+    ].map((variant) => ({
+      label: `${variant}`,
+      class: `pseudo-${variant}`,
+      modelValue: false,
+    })),
+    {
+      label: 'focus',
+      class: 'pseudo-focus-within',
+      modelValue: false,
+    },
+  ],
+};
+
+export const Checked: StoryObj = { render: () => (BasicStories) };
+Checked.decorators = [ withVariants ];
+Checked.parameters = {
+  variants: [
+    {
+      label: 'default',
+      modelValue: true,
+    },
+    ...[
+      'hover',
+      'active',
+    ].map((variant) => ({
+      label: `${variant}`,
+      class: `pseudo-${variant}`,
+      modelValue: true,
+    })),
+    {
+      label: 'focus',
+      class: 'pseudo-focus-within',
+      modelValue: true,
+    },
+  ],
 };
--- src/components/atoms/UiRadio/UiRadio.stories.ts
+++ src/components/atoms/UiRadio/UiRadio.stories.ts
@@ -4,8 +4,12 @@ import type {
 } from '@storybook/vue3';
 import { UiRadio } from '@index';
 import type { RadioProps } from '@index';
-import { withVModel } from '@sb/decorators';
+import {
+  withVariants,
+  withVModel,
+} from '@sb/decorators';
 import { getArgTypes } from '@sb/helpers';
+import { content } from '@sb/helpers/argTypes/index';
 import {
   BasicStories,
   BasicStoriesSource,
@@ -28,20 +32,22 @@ const meta = {
   title: 'Atoms/Radio',
   component: UiRadio,
   args: {
-    ...args,
     modelValue: false,
-    content: 'I read and accept Terms of Service and Privacy Policy.',
+    content: 'I’m overweight or obese',
     class: [],
-    value: '',
+    value: 'overweight-or-obese',
     id: '',
     disabled: false,
     inputAttrs: { 'data-testid': 'input-element' },
     iconCheckmarkAttrs: { 'data-testid': 'icon-checkmark' },
     textLabelAttrs: { 'data-testid': 'text-label' },
+    ...args,
   },
   argTypes: {
     ...argTypes,
-    modelValue: { control: 'boolean' },
+    content,
+    modelValue: { control: 'text' },
+    value: { control: 'text' },
   },
   parameters: { chromatic: { disableSnapshot: false } },
 } satisfies Meta;
@@ -52,4 +58,52 @@ Basic.decorators = [ withVModel ];
 Basic.parameters = {
   docs: { source: { code: BasicStoriesSource } },
   chromatic: { disableSnapshot: true },
+};
+
+export const Unchecked: StoryObj = { render: () => (BasicStories) };
+Unchecked.decorators = [ withVariants ];
+Unchecked.parameters = {
+  variants: [
+    {
+      label: 'default',
+      modelValue: '',
+    },
+    ...[
+      'hover',
+      'active',
+    ].map((variant) => ({
+      label: `${variant}`,
+      class: `pseudo-${variant}`,
+      modelValue: '',
+    })),
+    {
+      label: 'focus',
+      class: 'pseudo-focus-within',
+      modelValue: '',
+    },
+  ],
+};
+
+export const Checked: StoryObj = { render: () => (BasicStories) };
+Checked.decorators = [ withVariants ];
+Checked.parameters = {
+  variants: [
+    {
+      label: 'default',
+      modelValue: 'overweight-or-obese',
+    },
+    ...[
+      'hover',
+      'active',
+    ].map((variant) => ({
+      label: `${variant}`,
+      class: `pseudo-${variant}`,
+      modelValue: 'overweight-or-obese',
+    })),
+    {
+      label: 'focus',
+      class: 'pseudo-focus-within',
+      modelValue: 'overweight-or-obese',
+    },
+  ],
 };
