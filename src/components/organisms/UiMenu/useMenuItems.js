305abe99a5dde2f037b6b172d02fb3c5884203f8
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2024-01-25 12:25:37 +0100

    fix: import by alias in useMenuItems

--- src/components/organisms/UiMenu/useMenuItems.js
+++ src/components/organisms/UiMenu/useMenuItems.js
@@ -1,5 +1,5 @@
 import { computed } from 'vue';
-import { useActiveElement } from '@/composable';
+import { useActiveElement } from '../../../composable';
 
 export default function useMenuItems(menuItems) {
   const activeElement = useActiveElement();
