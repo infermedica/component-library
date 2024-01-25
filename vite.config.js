c32a79757d9c68022b0f8f9ce9a1155d6832f0fe
Przemek Spaczek <przemyslaw.spaczek@infermedica.com>
2024-01-25 20:46:40 +0100

    fix: import by alias in UIHorizontalPaging (#344)
    * chore: remove aliases from playground app
    
    * fix: import by alias in UiHorizontalPaging

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src'),
-      },
-      {
-        find: '@sb',
-        replacement: path.resolve(__dirname, '.storybook'),
-      },
-      {
-        find: '@index',
-        replacement: path.resolve(__dirname, './index.ts'),
-      },
-    ],
-  },
   plugins: [
     vue(),
     svgLoader({
