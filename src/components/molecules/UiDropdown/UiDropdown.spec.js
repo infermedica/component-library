7ca4adddda046c07d9d3e30e376cbb76f3cf1ea2
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2024-02-01 12:22:45 +0100

    fix: remove new UiDropdown

--- src/components/molecules/UiDropdown/UiDropdown.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import UiDropdown from './UiDropdown.vue';
-
-describe('UiDropdown.vue', () => {
-  test('renders a component', () => {
-    const wrapper = mount(UiDropdown);
-    expect(wrapper.classes('ui-dropdown')).toBe(true);
-  });
-});
