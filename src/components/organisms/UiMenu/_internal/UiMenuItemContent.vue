ad34e8838b3c081623a516680c15aaa25f939005
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2024-05-07 15:21:45 +0200

    fix: use MenuRenderItem instead any

--- src/components/organisms/UiMenu/_internal/UiMenuItemContent.vue
+++ src/components/organisms/UiMenu/_internal/UiMenuItemContent.vue
@@ -6,9 +6,10 @@
 
 <script setup lang="ts">
 import UiText from '../../../atoms/UiText/UiText.vue';
+import type { MenuRenderItem } from '../UiMenu.vue';
 
 export interface ListItemContentProps {
-  item?: any
+  item?: MenuRenderItem
 }
 
 const props = withDefaults(defineProps<ListItemContentProps>(), { item: () => ({ name: '' }) });
